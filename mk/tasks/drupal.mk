6318fd90b00081862a8422ba1bb213bfcce3c1a4
Christopher Gervais <chris@ergonlogic.com>
2016-03-16 11:06:38 -0400

    Fix up drupal install.

--- mk/tasks/drupal.mk
+++ mk/tasks/drupal.mk
@@ -1,10 +1,12 @@
-makefile     = dev.build.yml
-root         = ~/drupal
-root_exists  = $(shell if [[ -d $(root) ]]; then echo 1; fi)
-site_exists  = $(shell if [[ -f $(root)/sites/default/settings.php ]]; then echo 1; fi)
-profile      = make_server
-port         = 8888
-uri          = http://localhost:$(port)
+DRUSH_MAKEFILE ?= $(FILES_DIR)/dev.build.yml
+root           ?= ~/drupal
+root_exists    ?= $(shell if [[ -d $(root) ]]; then echo 1; fi)
+site_exists    ?= $(shell if [[ -f $(root)/sites/default/settings.php ]]; then echo 1; fi)
+PROJECT_NAME   ?= example
+PROJECT_TYPE   ?= module
+PROFILE        ?= $(shell if [[ '$(PROJECT_TYPE)' == 'profile' ]]; then echo '$(PROJECT_NAME)'; else echo 'minimal'; fi)
+port           ?= 8888
+uri            ?= http://localhost:$(port)
 
 help-drupal:
 	@echo "make drupal"
@@ -13,17 +15,17 @@ help-drupal:
 	@echo "  Stop the server running started during site install."
 
 rebuild-platform: $(root)
-$(root): $(ROOT_DIR)/$(makefile)
+$(root): $(DRUSH_MAKEFILE)
 
-build-platform: vm
+build-platform:
 ifneq '$(root_exists)' '1'
-	@$(drush) -y make $(ROOT_DIR)/$(makefile) $(root)
-	@ln -s $(ROOT_DIR) $(root)/profiles/$(profile)
+	@$(drush) -y make $(DRUSH_MAKEFILE) $(root)
+	@ln -s $(PROJECT_ROOT) $(root)/$(PROJECT_TYPE)s/$(PROJECT_NAME)
 endif
 
 drupal: kill-server build-platform
 ifneq '$(site_exists)' '1'
-	@cd $(root) && $(drush) -y site-install $(profile) --db-url=mysql://root@localhost/site0 --account-pass=pwd
+	@cd $(root) && $(drush) -y site-install $(PROFILE) --db-url=mysql://root@localhost/site0 --account-pass=pwd
 endif
 	@echo "<?php" > ~/.drush/local.alias.drushrc.php
 	@echo "  \$$aliases['local'] = array('root' => '$(root)','uri' => '$(uri)');" >> ~/.drush/local.alias.drushrc.php
