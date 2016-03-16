c43ad7fb8f20a1b67a734bc69cb9658f29007ff1
Christopher Gervais <chris@ergonlogic.com>
2016-03-16 11:07:44 -0400

    Add vagrant help text.

--- mk/tools/vagrant.mk
+++ mk/tools/vagrant.mk
@@ -1,9 +1,19 @@
+help-vagrant:
+	@echo "make vagrant"
+	@echo "  Add a Vagrantfile."
+	@echo "make up"
+	@echo "  Launch Vagrant."
+	@echo "make rebuild"
+	@echo "  Destroy the current Vagrant VM and re-provision a new one."
+
 vagrant: Vagrantfile
+
 Vagrantfile:
 	@ln -s $(MK_DIR)/Vagrantfile .
 
 up:
 	@vagrant up
+
 rebuild:
 	@vagrant destroy -f && vagrant up
 
