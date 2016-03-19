1be1643a33257da0d49546438a434cdd8f782ac3
Christopher Gervais <chris@ergonlogic.com>
2016-03-19 01:34:36 -0400

    Update drupal.

--- mk/tasks/drupal.mk
+++ mk/tasks/drupal.mk
@@ -1,49 +1,57 @@
-DRUSH_MAKEFILE ?= $(FILES_DIR)/dev.build.yml
-root           ?= ~/drupal
-root_exists    ?= $(shell if [[ -d $(root) ]]; then echo 1; fi)
-site_exists    ?= $(shell if [[ -f $(root)/sites/default/settings.php ]]; then echo 1; fi)
+drush_make     ?= $(drush) -y make --working-copy
+DRUSH_MAKEFILE ?= $(PROJECT_ROOT)/dev.build.yml
+SITE           ?= default
+PLATFORM_ROOT  ?= ~/drupal-$(SITE)
+site_exists    ?= $(shell if [[ -f $(PLATFORM_ROOT)/sites/default/settings.php ]]; then echo 1; fi)
 PROJECT_NAME   ?= example
 PROJECT_TYPE   ?= module
 PROFILE        ?= $(shell if [[ '$(PROJECT_TYPE)' == 'profile' ]]; then echo '$(PROJECT_NAME)'; else echo 'minimal'; fi)
-port           ?= 8888
-uri            ?= http://localhost:$(port)
+PHP_SERVER_PORT ?= 8888
+SITE_URI       ?= http://localhost:$(PHP_SERVER_PORT)
+
+.PHONY: help-drupal rebuild-platform build-platform drupal kill-server drupal-behat-config
 
 help-drupal:
 	@echo "make drupal"
 	@echo "  Build a Drupal codebase, install a site and start a web server."
 	@echo "make kill-server"
-	@echo "  Stop the server running started during site install."
+	@echo "  Stop the server running started dSITE_URIng site install."
 
-rebuild-platform: $(root)
-$(root): $(DRUSH_MAKEFILE)
+build-platform: $(PLATFORM_ROOT)
+$(PLATFORM_ROOT): $(DRUSH_MAKEFILE)
+$(DRUSH_MAKEFILE):
+	@echo "Initializing build makefile."
+	@cp $(FILES_DIR)/dev.build.yml $(DRUSH_MAKEFILE)
+$(PLATFORM_ROOT):
+	@echo "Building platform using $(DRUSH_MAKEFILE)."
+	@$(drush_make) $(DRUSH_MAKEFILE) $(PLATFORM_ROOT)
+	@ln -s $(PROJECT_ROOT) $(PLATFORM_ROOT)/$(PROJECT_TYPE)s/$(PROJECT_NAME)
 
-build-platform:
-ifneq '$(root_exists)' '1'
-	@$(drush) -y make $(DRUSH_MAKEFILE) $(root)
-	@ln -s $(PROJECT_ROOT) $(root)/$(PROJECT_TYPE)s/$(PROJECT_NAME)
-endif
+rebuild-platform:
+	@echo "Rebuilding platform using $(DRUSH_MAKEFILE)."
+	@cd $(PLATFORM_ROOT) && \
+  $(drush_make) $(DRUSH_MAKEFILE)
+	@ln -sf $(PROJECT_ROOT) $(PLATFORM_ROOT)/$(PROJECT_TYPE)s/$(PROJECT_NAME)
 
-drupal: kill-server build-platform
-ifneq '$(site_exists)' '1'
-	@cd $(root) && $(drush) -y site-install $(PROFILE) --db-url=mysql://root@localhost/site0 --account-pass=pwd
-endif
+drupal: kill-server build-platform $(PLATFORM_ROOT)/sites/$(SITE)/settings.php
+	@cd $(PLATFORM_ROOT) && $(drush) -y site-install $(PROFILE) --db-url=mysql://PLATFORM_ROOT@localhost/site0 --account-pass=pwd
 	@echo "<?php" > ~/.drush/local.alias.drushrc.php
-	@echo "  \$$aliases['local'] = array('root' => '$(root)','uri' => '$(uri)');" >> ~/.drush/local.alias.drushrc.php
+	@echo "  \$$aliases['$(SITE)'] = array('PLATFORM_ROOT' => '$(PLATFORM_ROOT)','SITE_URI' => '$(SITE_URI)');" >> ~/.drush/$(SITE).alias.drushrc.php
 	@echo "Starting PHP server."
-	@cd $(root) && php -S 0.0.0.0:$(port) &> ~/runserver.log &
+	@cd $(PLATFORM_ROOT) && php -S 0.0.0.0:$(PHP_SERVER_PORT) &> ~/runserver.log &
 
 kill-server:
 	@echo "Stopping PHP server."
 	@ps aux|grep [p]hp > /dev/null || pkill -f php
+	@echo "Waiting for PHP server to stop."
 	@sleep 3
-
 
 make:
 	@vagrant ssh -c"cd /var/www/html/d8 && sudo drush -y make /vagrant/dev.build.yml"
 
-drupal-behat-config: drush-bde-env $(root)
+drupal-behat-config: drush-bde-env $(PLATFORM_ROOT)
 	@echo Generating project-specific Behat config.
-	@cd $(root) && $(drush) beg --subcontexts=profiles/$(PROFILE)/modules --site-root=$(root) --skip-path-check --base-url=$(uri) $(PROJECT_ROOT)/behat_params.sh
+	@cd $(PLATFORM_ROOT) && $(drush) beg --subcontexts=profiles/$(PROFILE)/modules --site-PLATFORM_ROOT=$(PLATFORM_ROOT) --skip-path-check --base-url=$(SITE_URI) $(PROJECT_ROOT)/behat_params.sh
 
 
 # vi:syntax=makefile
