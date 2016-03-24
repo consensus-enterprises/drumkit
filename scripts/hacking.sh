9508069bfbfac5fdbf0da8f0c2a53bcc90cd3888
Christopher Gervais <chris@ergonlogic.com>
2016-03-24 02:04:43 -0400

    Provide guidance on hcking.sh usage.

--- scripts/hacking.sh
+++ scripts/hacking.sh
@@ -1,2 +1,7 @@
-CWD=`pwd`
-export PATH="$CWD/.local/bin:$PATH"
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "This script must be sourced. Try \". /path/to/hacking.sh\"" 1>&2
+  exit 1
+else
+  CWD=`pwd`
+  export PATH="$CWD/.local/bin:$PATH"
+fi
