cc320296166af7b75b778410fac7b11b76af8c29
Christopher Gervais <chris@ergonlogic.com>
2016-03-30 00:53:02 -0400

    Remove failure on Linux.

--- mk/tools/os_pkg.mk
+++ mk/tools/os_pkg.mk
@@ -1,6 +1,8 @@
 OS = $(shell uname)
 ifeq ($(OS), Darwin)
   include $(MK_DIR)/mk/tools/os_pkg/dmg.mk
+else ifeq ($(OS), Linux)
+  #include $(MK_DIR)/mk/tools/os_pkg/deb.mk
 else
   $(error '$(OS)' is not a supported operating system)
 endif
