693661a40810e255f8dabcc93f693f6cbb23504b
Christopher Gervais <chris@ergonlogic.com>
2016-04-06 08:52:01 -0400

    Add utility target to completely remove git submodules.

--- mk/tasks/utils.mk
+++ mk/tasks/utils.mk
@@ -20,4 +20,15 @@ deps-utils:
 	@echo Installing some utilities
 	@sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install $(utils)
 
+git-rm-submodule-help:
+	@echo "make git-rm-submodule SUBMODULE=<git submodule path>"
+	@echo "  Completely remove the git submodule registered at <git submodule path>"
+
+git-rm-submodule:
+	@echo Removing git submodule at $(SUBMODULE)
+	if git submodule status "$(SUBMODULE)" >/dev/null 2>&1; then git submodule deinit $(SUBMODULE); git rm -f $(SUBMODULE); fi
+	if [ -d .git/modules/$(SUBMODULE) ]; then rm -rf .git/modules/$(SUBMODULE); fi
+	git config -f .gitmodules --remove-section "submodule.$(SUBMODULE)"
+	if [ -z "$$(cat .gitmodules)" ]; then git rm -f .gitmodules; else git add .gitmodules; fi
+
 # vi:syntax=makefile
