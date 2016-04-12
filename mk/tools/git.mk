88a85c82bb32a3315fe99c919db7d6257888a8d8
Christopher Gervais <chris@ergonlogic.com>
2016-04-12 13:16:21 -0400

    Add link to latest git sources, so that we'll have a stable path regardless of versions.

--- mk/tools/git.mk
+++ mk/tools/git.mk
@@ -9,20 +9,24 @@ gits: $(1)
 debug-gits: debug-$(1)
 debug-$(1):
 	@echo NAME: $$($(1)_NAME)
-	@echo RELEASE: $$($(1)_RELEASE)
 	@echo DOWNLOAD_URL: $$($(1)_DOWNLOAD_URL)
 	@echo DEPENDENCIES: $$($(1)_DEPENDENCIES)
 	@echo PARENT: $$($(1)_PARENT)
 	@echo EXECUTABLE: $(BIN_DIR)/$(1)
-	@echo SOURCE: $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE)
+	@echo SOURCE: $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-latest
 	@echo command w/options: $(1) $$($(1)_OPTIONS)
+ifeq ($(1), $$($(1)_PARENT))
+	@echo RELEASE: $$($(1)_RELEASE)
+endif
 	@echo
 
 tools-help-$(1):
 	@echo "make $(1)"
 	@echo "  Install $$($(1)_NAME)."
+ifeq ($(1), $$($(1)_PARENT))
 	@echo "make $(1) $(1)_RELEASE=$$($(1)_RELEASE)"
 	@echo "  Install the $$($(1)_RELEASE) release of $$($(1)_NAME)."
+endif
 
 clean-gits: clean-$(1)
 clean-$(1):
@@ -46,20 +50,27 @@ $$($(1)_PARENT): $(1)
 clean-$$($(1)_PARENT): clean-$(1)
 endif
 
-$(BIN_DIR)/$(1): $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE)/$$($(1)_BIN_DIR)/$(1)
+$(BIN_DIR)/$(1): $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-latest/$$($(1)_BIN_DIR)/$(1)
 	@echo Installing $$($(1)_NAME).
-	@ln -sf $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE)/$$($(1)_BIN_DIR)/$(1) $(BIN_DIR)/$(1)
+	@cd $(BIN_DIR) && \
+  ln -sf ../src/$$($(1)_PARENT)/$$($(1)_PARENT)-latest/$$($(1)_BIN_DIR)/$(1) $(1)
 	@chmod a+x $(BIN_DIR)/$(1)
 ifeq ($(1), $$($(1)_PARENT))
-	@cd $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE) && \
+	@cd $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-latest && \
 	$$($(1)_COMMAND) 2&>1 > /dev/null && \
 	. $(MK_DIR)/scripts/hacking.sh && \
   $(1) --version
 endif
 
-$(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE)/$$($(1)_BIN_DIR)/$(1): $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE)
+$(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-latest/$$($(1)_BIN_DIR)/$(1): $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-latest
 
 ifeq ($(1), $$($(1)_PARENT))
+
+$(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-latest: $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE)
+	@echo Symlinking the $$($(1)_RELEASE) release of $$($(1)_NAME) to $$($(1)_PARENT)-latest.
+	@cd $(SRC_DIR)/$$($(1)_PARENT)/ && \
+  ln -s $$($(1)_PARENT)-$$($(1)_RELEASE) $$($(1)_PARENT)-latest
+
 $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE):
 	@echo Downloading the $$($(1)_RELEASE) release of $$($(1)_NAME) via Git.
 	@git clone --quiet --depth 1 $$($(1)_DOWNLOAD_URL) $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE)
@@ -67,6 +78,7 @@ $(SRC_DIR)/$$($(1)_PARENT)/$$($(1)_PARENT)-$$($(1)_RELEASE):
 	git fetch --quiet --tags && \
 	git checkout --quiet $$($(1)_RELEASE) && \
 	git submodule update --quiet --init
+
 endif
 
 endef
