4c84e1e3199611e9521fb5cfc3609d099e9b0498
Christopher Gervais <chris@ergonlogic.com>
2016-04-27 08:46:29 -0400

    Fix path to env setup script.

--- scripts/hacking.sh
+++ scripts/hacking.sh
@@ -1,5 +1,5 @@
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  echo "This script must be sourced. Try \". /path/to/hacking.sh\"" 1>&2
+  echo "This script must be sourced. Try \". ${BASH_SOURCE[0]}\"" 1>&2
   exit 1
 else
   CWD=`pwd`
