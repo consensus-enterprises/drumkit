048e92eeb70cc02cfeb675274a89ebe62a6c0f83
Christopher Gervais <chris@ergonlogic.com>
2016-04-27 15:07:18 -0400

    Add further Ansible targets.

--- mk/tools/git/ansible-galaxy.mk
+++ mk/tools/git/ansible-galaxy.mk
@@ -6,15 +6,19 @@ ansible-galaxy_BIN_DIR      = bin
 ansible-galaxy_PARENT       = ansible
 
 ANSIBLE_REQUIREMENTS       ?= roles/requirements.yml
+ANSIBLE_ROLES_PATH         ?= roles
 
 $(ANSIBLE_REQUIREMENTS):
 	@mkdir -p $(dir $(ANSIBLE_REQUIREMENTS))
 	echo "---" >> $(ANSIBLE_REQUIREMENTS)
 
-ansible-roles: $(ANSIBLE_REQUIREMENTS)
+$(ANSIBLE_ROLES_PATH):
+	@mkdir -p $(ANSIBLE_ROLES_PATH)
+
+ansible-roles: $(ANSIBLE_REQUIREMENTS) $(ANSIBLE_ROLES_PATH)
 	ansible-galaxy install -i -r $(ANSIBLE_REQUIREMENTS)
 
-ansible-roles-quiet: $(ANSIBLE_REQUIREMENTS)
-	@ansible-galaxy install -i -r $(ANSIBLE_REQUIREMENTS) > /dev/null
+ansible-roles-quiet: $(ANSIBLE_REQUIREMENTS) $(ANSIBLE_ROLES_PATH)
+	@ansible-galaxy install -i -r $(ANSIBLE_REQUIREMENTS) -p $(ANSIBLE_ROLES_PATH) > /dev/null
 
 # vi:syntax=makefile
--- mk/tools/git/ansible-playbook.mk
+++ mk/tools/git/ansible-playbook.mk
@@ -4,4 +4,19 @@ ansible-playbook_DOWNLOAD_URL = https://github.com/ansible/ansible.git
 ansible-playbook_DEPENDENCIES = python-paramiko python-pip python-yaml python-jinja2 python-pycurl
 ansible-playbook_BIN_DIR      = bin
 ansible-playbook_PARENT       = ansible
+
+ANSIBLE_PLAYBOOK  ?= tests/playbook.yml
+ANSIBLE_INVENTORY ?= tests/inventory
+
+ansible-playbook-test: ansible-roles
+	@echo "Check the role/playbook's syntax."
+	ansible-playbook -i $(ANSIBLE_INVENTORY) $(ANSIBLE_PLAYBOOK) --syntax-check
+	@echo "Run the role/playbook with ansible-playbook."
+	ansible-playbook -i $(ANSIBLE_INVENTORY) $(ANSIBLE_PLAYBOOK) --connection=local --sudo
+	@echo "Run the role/playbook again, checking to make sure it's idempotent."
+	ansible-playbook -i $(ANSIBLE_INVENTORY) $(ANSIBLE_PLAYBOOK) --connection=local --sudo \
+  | grep -q 'changed=0.*failed=0' \
+  && (echo 'Idempotence test: pass' && exit 0) \
+  || (echo 'Idempotence test: fail' && exit 1)
+
 # vi:syntax=makefile
