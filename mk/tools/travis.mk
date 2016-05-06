05aef485d4e9b3c85238836bccd8614b5166ab59
Christopher Gervais <chris@ergonlogic.com>
2016-05-06 13:27:22 -0400

    Improve Travis install a bit; still needs work.

--- mk/tools/travis.mk
+++ mk/tools/travis.mk
@@ -1,4 +1,5 @@
-ruby:
-	brew install ruby
 travis: ruby
-	gem install travis -v 1.8.2 --no-rdoc --no-ri
+	sudo gem install travis -v 1.8.2 --no-rdoc --no-ri
+ruby: /usr/bin/ruby
+/usr/bin/ruby:
+	brew install ruby
