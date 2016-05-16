3f07ec011ef6732135d85b719bc69dceef35013e
Christopher Gervais <chris@ergonlogic.com>
2016-05-16 19:00:51 -0400

    Update Ansible version.

--- mk/tools/git/ansible-doc.mk
+++ mk/tools/git/ansible-doc.mk
@@ -1,5 +1,5 @@
 ansible-doc_NAME         = Ansible Doc
-ansible-doc_RELEASE      = v2.0.1.0-1
+ansible-doc_RELEASE      = v2.0.2.0-1
 ansible-doc_DOWNLOAD_URL = https://github.com/ansible/ansible.git
 ansible-doc_DEPENDENCIES = python-paramiko python-pip python-yaml python-jinja2 python-pycurl
 ansible-doc_BIN_DIR      = bin
--- mk/tools/git/ansible-galaxy.mk
+++ mk/tools/git/ansible-galaxy.mk
@@ -1,5 +1,5 @@
 ansible-galaxy_NAME         = Ansible Galaxy
-ansible-galaxy_RELEASE      = v2.0.1.0-1
+ansible-galaxy_RELEASE      = v2.0.2.0-1
 ansible-galaxy_DOWNLOAD_URL = https://github.com/ansible/ansible.git
 ansible-galaxy_DEPENDENCIES = python-paramiko python-pip python-yaml python-jinja2 python-pycurl
 ansible-galaxy_BIN_DIR      = bin
