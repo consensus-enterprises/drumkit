f8c3036b1fe99d2d8725114accea0d6c08d0b239
Christopher Gervais <chris@ergonlogic.com>
2016-05-31 20:03:06 -0400

    Bump Drush version.

--- mk/tools/phar/drush.mk
+++ mk/tools/phar/drush.mk
@@ -1,6 +1,6 @@
 drush_NAME         = Drush
 drush_OPTIONS      = --include=$(MK_DIR)/.local/drush
-drush_RELEASE      = 8.0.5
+drush_RELEASE      = 8.1.2
 drush_DOWNLOAD_URL = https://github.com/drush-ops/drush/releases/download/$(drush_RELEASE)/drush.phar
 drush_DEPENDENCIES = git php5-mysql php5-cli php5-gd
 
