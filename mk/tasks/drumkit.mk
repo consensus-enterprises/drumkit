c9a326598327c31361bd0b21159df9f0bfc97fef
Christopher Gervais <chris@ergonlogic.com>
2016-06-05 00:53:31 -0400

    Add update-drumkit target.

---
+++ mk/tasks/drumkit.mk
@@ -0,0 +1,21 @@
+update-drumkit: update-drumkit-prompt
+	@if [ $(DRUMKIT_CONFIRM) == 'y' ]; then echo; else echo; echo "Cancelled."; exit 1; fi
+	@echo "Updating Drumkit."
+	@cd $(MK_DIR) && git pull
+	@git add .mk
+	@git commit -m"Update Drumkit."
+update-drumkit: DRUMKIT_CONFIRM ?= $(shell bash -c 'read -s -p "Proceed? (y/n):" CONFIRM; echo $$CONFIRM')
+
+update-drumkit-prompt: update-drumkit-check
+	@echo "You are about to update Drumkit in this project."
+	@echo "This will perform the following operations:"
+	@echo "  1. Pull the latest changes from Drumkit."
+	@echo "  2. Commit the changs to the Drumkit submodule."
+
+update-drumkit-check:
+	@if (( $(DRUMKIT_CLEAN) > 0 )); then echo "Drumkit directory has been changed. Cannot proceed."; exit 1; fi
+	@if (( $(DRUMKIT_CLEAN_ROOT) > 0 )); then echo "This repository contains staged changes. Please commit them, or unstage (reset) them. Cannot proceed."; exit 1; fi
+update-drumkit-check: DRUMKIT_CLEAN ?= $(shell bash -c 'git diff --name-only --exit-code .mk 2>&1 > /dev/null; echo $$?')
+update-drumkit-check: DRUMKIT_CLEAN_ROOT ?= $(shell bash -c 'git diff --cached --name-only --exit-code 2>&1 > /dev/null; echo $$?')
+
+# vi:syntax=makefile
