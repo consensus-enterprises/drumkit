07e1b379993a115bbca1ba8270518b551cf0bc28
Christopher Gervais <chris@ergonlogic.com>
2016-07-06 14:41:52 -0400

    Clean up drush variables.

--- mk/tools/phar/drush.mk
+++ mk/tools/phar/drush.mk
@@ -1,11 +1,11 @@
 drush_NAME         ?= Drush
-drush_OPTIONS      ?= --include=$(MK_DIR)/.local/drush
+drush_OPTIONS      ?= --include=$(DRUSH_DIR)
 drush_RELEASE      ?= 8.1.2
 drush_DOWNLOAD_URL ?= https://github.com/drush-ops/drush/releases/download/$(drush_RELEASE)/drush.phar
 drush_DEPENDENCIES ?= git php5-mysql php5-cli php5-gd
 
-DRUSH_DIR ?= $(shell echo ~/.drush)
-$(DRUSH_DIR):
+DRUSH_DIR ?= $(LOCAL_DIR)/drush
+$(DRUSH_DIR): $(LOCAL_DIR)
 	@echo "Ensuring Drush home directory ($(DRUSH_DIR)) exists."
 	@mkdir -p $(DRUSH_DIR)
 
