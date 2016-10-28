835dc1fc3f445b5a1b08ff7fec358402401e0ec4
Christopher Gervais <chris@ergonlogic.com>
2016-10-28 13:15:24 -0400

    Rename hacking.sh to bootstrap.sh

--- scripts/hacking.sh
+++ scripts/hacking.sh
@@ -1,12 +1,1 @@
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-  echo "This script must be sourced. Try \". ${BASH_SOURCE[0]}\"" 1>&2
-  exit 1
-else
-  CWD=`pwd`
-  if [[ -d .mk/ ]]; then
-    BIN_PATH="$CWD/.mk/.local/bin"
-  else
-    BIN_PATH="$CWD/.local/bin"
-  fi
-  export PATH="$BIN_PATH:$PATH"
-fi
+bootstrap.sh
