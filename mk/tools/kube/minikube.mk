944ead21d16bef1cbc6ee3b0d9c98432792a75dd
Christopher Gervais <chris@ergonlogic.com>
2016-10-28 10:58:49 -0400

    Update minikube recipe.

--- mk/tools/kube/minikube.mk
+++ mk/tools/kube/minikube.mk
@@ -1,7 +1,6 @@
 minikube_NAME         ?= minikube
-minikube_RELEASE      ?= v0.10.0
+minikube_RELEASE      ?= v0.12.0
 minikube_DOWNLOAD_URL ?= https://storage.googleapis.com/$(minikube_NAME)/releases/$(minikube_RELEASE)/$(minikube_NAME)-$(MK_OS)-amd64
 minikube_DEPENDENCIES ?= virtualbox
 
-minikube: kubectl
 # vi:syntax=makefile
