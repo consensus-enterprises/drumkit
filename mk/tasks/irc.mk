93a065fb274a1a7705f08b870c7f650c31190bf3
Christopher Gervais <chris@ergonlogic.com>
2016-11-04 13:58:12 -0400

    Make IRC notifications default to git status messages.

--- mk/tasks/irc.mk
+++ mk/tasks/irc.mk
@@ -2,7 +2,12 @@ IRC_USER    ?= drumkit
 IRC_SERVER  ?= irc.freenode.net
 IRC_PORT    ?= 6667
 IRC_CHANNEL ?= \#drumk.it
-IRC_MSG     ?= Testing Drumkit\'s irc-message target
+IRC_COMMIT  ?= "Commit: $$(git log -1 --oneline --no-decorate)"
+IRC_STATUS  ?= "succeeded"
+IRC_PROJECT ?= "drumkit"
+IRC_GIT_URL ?= "https://gitlab.com/$(IRC_PROJECT)/$$(basename $$(pwd))"
+IRC_REF_URL ?= "$(IRC_GIT_URL)/commit/$$(git rev-parse --short HEAD)/builds"
+IRC_MSG     ?= "\\e[32mBuild on \\e[33m$$(git rev-parse --abbrev-ref HEAD)\\e[32m $(IRC_STATUS).\\e[0m See $(IRC_REF_URL)"
 
 irc-message:
-	echo -e "NICK $(IRC_USER)\nUSER $(IRC_USER) 8 * : $(IRC_USER)\nJOIN $(IRC_CHANNEL)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_MSG)\nQUIT\n" | nc $(IRC_SERVER) $(IRC_PORT)
+	echo -e "NICK $(IRC_USER)\nUSER $(IRC_USER) 8 * : $(IRC_USER)\nJOIN $(IRC_CHANNEL)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_MSG)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_COMMIT)\nQUIT\n" | nc $(IRC_SERVER) $(IRC_PORT)
