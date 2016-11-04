e4e58a67857e34b562252f3fef01893232b3d196
Christopher Gervais <chris@ergonlogic.com>
2016-11-04 14:20:09 -0400

    Simplify CI IRC notifications.

--- mk/tasks/irc.mk
+++ mk/tasks/irc.mk
@@ -7,7 +7,16 @@ IRC_STATUS  ?= "succeeded"
 IRC_PROJECT ?= "drumkit"
 IRC_GIT_URL ?= "https://gitlab.com/$(IRC_PROJECT)/$$(basename $$(pwd))"
 IRC_REF_URL ?= "$(IRC_GIT_URL)/commit/$$(git rev-parse --short HEAD)/builds"
-IRC_MSG     ?= "\\e[32mBuild on \\e[33m$$(git rev-parse --abbrev-ref HEAD)\\e[32m $(IRC_STATUS).\\e[0m See $(IRC_REF_URL)"
+IRC_BRANCH  ?= "master"
+IRC_MSG     ?= "Test message from Drumkit."
+IRC_SUCCESS ?= "\\e[32mSuccess building \\e[33m$(IRC_BRANCH)\\e[32m branch.\\e[0m See $(IRC_REF_URL)"
+IRC_FAILURE ?= "\\e[31mFailure building \\e[33m$(IRC_BRANCH)\\e[31m branch.\\e[0m See: $(IRC_REF_URL)"
 
 irc-message:
-	echo -e "NICK $(IRC_USER)\nUSER $(IRC_USER) 8 * : $(IRC_USER)\nJOIN $(IRC_CHANNEL)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_MSG)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_COMMIT)\nQUIT\n" | nc $(IRC_SERVER) $(IRC_PORT)
+	echo -e "NICK $(IRC_USER)\nUSER $(IRC_USER) 8 * : $(IRC_USER)\nJOIN $(IRC_CHANNEL)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_MSG)\nQUIT\n" | nc $(IRC_SERVER) $(IRC_PORT)
+
+irc-ci-success:
+	echo -e "NICK $(IRC_USER)\nUSER $(IRC_USER) 8 * : $(IRC_USER)\nJOIN $(IRC_CHANNEL)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_SUCCESS)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_COMMIT)\nQUIT\n" | nc $(IRC_SERVER) $(IRC_PORT)
+
+irc-ci-failure:
+	echo -e "NICK $(IRC_USER)\nUSER $(IRC_USER) 8 * : $(IRC_USER)\nJOIN $(IRC_CHANNEL)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_FAILURE)\nPRIVMSG $(IRC_CHANNEL) : $(IRC_COMMIT)\nQUIT\n" | nc $(IRC_SERVER) $(IRC_PORT)
