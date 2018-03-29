2a472e96c2b4328f16606a2696a4354926724eb4
Christopher Gervais <chris@ergonlogic.com>
2018-03-29 13:15:23 -0400

    Fix matrix notification pipeline URL.

--- mk/tasks/matrix.mk
+++ mk/tasks/matrix.mk
@@ -1,7 +1,7 @@
 MATRIX_MESSAGE    ?= "Test message from Drumkit."
 MATRIX_CI_SUCCESS ?= "✅  <strong>Success</strong>"
 MATRIX_CI_FAILURE ?= "❎  <strong>Failure</strong>"
-MATRIX_CI_MESSAGE ?= "building <em><a href='$(CI_PROJECT_URL)/tree/$(CI_COMMIT_REF_NAME)'>$(CI_COMMIT_REF_NAME)</a></em> branch of <a href='$(CI_PROJECT_URL)'>$(CI_PROJECT_NAME)</a>. See <a href='$(CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID)'>Pipeline \#$(CI_PIPELINE_ID)</a>.<br />Commit: <a href='$(CI_PROJECT_URL)/commit/$(CI_COMMIT_SHA)'>$$(git rev-parse --short HEAD)</a>: $$(git log -1 --pretty=%B)"
+MATRIX_CI_MESSAGE ?= "building <em><a href='$(CI_PROJECT_URL)/tree/$(CI_COMMIT_REF_NAME)'>$(CI_COMMIT_REF_NAME)</a></em> branch of <a href='$(CI_PROJECT_URL)'>$(CI_PROJECT_NAME)</a>. See <a href='$(CI_PROJECT_URL)/pipelines/$(CI_PIPELINE_ID)'>Pipeline \#$(CI_PIPELINE_ID)</a>.<br />Commit: <a href='$(CI_PROJECT_URL)/commit/$(CI_COMMIT_SHA)'>$$(git rev-parse --short HEAD)</a>: $$(git log -1 --pretty=%B)"
 
 matrix-message:
 	@echo $(MATRIX_MESSAGE) | $(MK_DIR)/scripts/mcat.py --stdin
