032eb7a4dd817b154224edbb2b55dd83d1081914
lamech <danieljasonfriedman@gmail.com>
2019-07-11 19:36:25 +0000

    Add hugo doc site facility

---
+++ mk/tasks/docs.mk
@@ -0,0 +1,11 @@
+# Build and serve our docs site locally.
+
+.PHONY: docs docs-kill
+
+HUGO_SERVE_CMD=hugo serve --noHTTPCache --quiet --disableFastRender
+
+docs: ## Run Hugo server locally in the background (http://localhost:1313).
+	cd docs && $(HUGO_SERVE_CMD) 2>&1 > /dev/null &
+
+docs-kill: ## Shut down the Hugo server.
+	pkill -xfe "$(HUGO_SERVE_CMD)"
