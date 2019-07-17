43dbe998bf9b066a5260c1de12f434da8e907049
lamech <danieljasonfriedman@gmail.com>
2019-07-17 20:01:15 +0000

    Dependencies.

--- mk/tasks/project/infra-openstack.mk
+++ mk/tasks/project/infra-openstack.mk
@@ -1,8 +1,8 @@
 # Tasks for initializing infra-openstack projects.
-init-infra-openstack: behat.yml features/testing.feature ansible.cfg
-	echo "Initializing Drumkit Openstack Infrastructure project"
-	git submodule add git@gitlab.com:consensus.enterprises/ansible-roles/ansible-role-cloud-openstack roles/consensus.cloud-openstack
-	echo "include roles/consensus.cloud-openstack/Makefile" >> $(PROJECT_ROOT)/Makefile
-	make init-cloud-openstack
+init-infra-openstack: behat ansible
+	@echo "Initializing Drumkit Openstack Infrastructure project."
+	@git submodule add git@gitlab.com:consensus.enterprises/ansible-roles/ansible-role-cloud-openstack roles/consensus.cloud-openstack
+	@echo "include roles/consensus.cloud-openstack/Makefile" >> $(PROJECT_ROOT)/Makefile
+	@make init-cloud-openstack
 
 # TODO: turn on/off relevant makefiles by adding symlinks inside a new makefiles.d
