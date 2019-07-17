8c6e0949700f459efba854c00f6380979be684ac
lamech <danieljasonfriedman@gmail.com>
2019-07-17 19:08:30 +0000

    Update ansible version

--- mk/tools/git/ansible-pull.mk
+++ mk/tools/git/ansible-pull.mk
@@ -1,5 +1,5 @@
 ansible-pull_NAME         = Ansible Pull
-ansible-pull_RELEASE      = v2.0.2.0-1
+ansible-pull_RELEASE      = v2.8.1
 ansible-pull_DOWNLOAD_URL = https://github.com/ansible/ansible.git
 ansible-pull_DEPENDENCIES = python-paramiko python-pip python-yaml python-jinja2 python-pycurl
 ansible-pull_BIN_DIR      = bin
--- mk/tools/git/ansible.mk
+++ mk/tools/git/ansible.mk
@@ -1,5 +1,5 @@
 ansible_NAME         = Ansible
-ansible_RELEASE      ?= v2.0.2.0-1
+ansible_RELEASE      ?= v2.8.1
 ansible_DOWNLOAD_URL = https://github.com/ansible/ansible.git
 ansible_DEPENDENCIES = python-paramiko python-pip python-yaml python-jinja2 python-pycurl
 ansible_BIN_DIR      = bin
