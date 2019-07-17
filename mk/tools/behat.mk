b4dcf18ea0d3cfa66e0ab86a1812cf9083060d2a
lamech <danieljasonfriedman@gmail.com>
2019-07-17 19:19:07 +0000

    Dependency for features/testing.feature

--- mk/tools/behat.mk
+++ mk/tools/behat.mk
@@ -27,7 +27,7 @@ clean-behat:
 	@rm -f $(BEHAT_BIN)
 
 install-behat: init-mk composer $(BEHAT_BIN)
-behat: install-behat behat.yml features features/testing.feature
+behat: install-behat behat.yml features/testing.feature
 
 behat.yml:
 	@cp $(FILES_DIR)/behat/project.behat.yml $(PROJECT_ROOT)/behat.yml
@@ -35,7 +35,7 @@ behat.yml:
 features:
 	@mkdir -p $(PROJECT_ROOT)/features
 
-features/testing.feature:
+features/testing.feature: features
 	@cp $(FILES_DIR)/behat/testing.feature $(PROJECT_ROOT)/features
 
 $(BEHAT_SRC)/composer.json: $(FILES_DIR)/behat/composer.json
