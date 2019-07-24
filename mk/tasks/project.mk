da1d7546853d9862c1d75f19283f606e4332665f
lamech <danieljasonfriedman@gmail.com>
2019-07-24 18:13:20 +0000

    Handling of mk.d and dynamic submodule hash initialization

--- mk/tasks/project.mk
+++ mk/tasks/project.mk
@@ -1,2 +1,30 @@
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+MK_D = $(PROJECT_ROOT)/mk.d
+MK_D_EXISTS ?= $(shell if [[ -d $(MK_D) ]]; then echo 1; fi)
+MK_D_NONEMPTY ?= $(shell if [[ `ls -A $(MK_D)` ]]; then echo 1; fi)
+
+$(MK_D) mk.d:
+	@mkdir -p $(MK_D)
+
 include $(SELF_DIR)project/*.mk
+
+ifeq ($(MK_D_EXISTS), 1)
+  ifeq ($(MK_D_NONEMPTY), 1)
+    include $(MK_D)/*.mk
+  endif
+endif
+
+# $1 = config file path
+# $2 = name of hash to initialize
+define get_submodules
+  $(shell awk '/^[^\#]/ {OFS=""; print "$$(call set,$(2),",$$1,",",$$2,")"}' $(1))
+endef
+
+define print_submodules
+  $(info $(call get_submodules,$(1),$(2)))
+endef
+
+define initialize_submodules_hash
+  $(eval $(call get_submodules,$(1),$(2)))
+endef
