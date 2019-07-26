42043c8ed3b6e7e600b272e877f51940149182c5
lamech <danieljasonfriedman@gmail.com>
2019-07-26 15:06:19 +0000

    Moved example file to the role

--- mk/tasks/project/aegir.mk
+++ mk/tasks/project/aegir.mk
@@ -2,7 +2,7 @@
 
 init-project-aegir-intro:
 	@echo "Initializing Drumkit Aegir project."
-init-project-aegir-real: init-project-aegir-intro $(MK_D) roles/consensus.aegir-policy $(MK_D)/consensus.aegir-policy.mk playbooks/groups/example-aegir-policy-group.yml
+init-project-aegir-real: init-project-aegir-intro $(MK_D) roles/consensus.aegir-policy $(MK_D)/consensus.aegir-policy.mk 
 	@make -s init-aegir-policy
 	@echo "Finished initializing Drumkit Aegir project."
 
@@ -13,7 +13,3 @@ $(MK_D)/consensus.aegir-policy.mk:
 
 roles/consensus.aegir-policy:
 	@git submodule add $(CONSENSUS_GIT_URL_BASE)/ansible-roles/ansible-role-aegir-policy roles/consensus.aegir-policy
-
-playbooks/groups/example-aegir-policy-group.yml:
-	@mkdir -p $(PROJECT_ROOT)/playbooks/groups
-	@cp $(FILES_DIR)/aegir-policy/example-aegir-policy-group.yml $(PROJECT_ROOT)/playbooks/groups
