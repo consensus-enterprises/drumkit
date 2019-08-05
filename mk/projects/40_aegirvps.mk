91effdb34a0170501d4f5d9a07d74ffe28a5d309
Christopher Gervais <chris@ergonlogic.com>
2019-08-05 21:10:15 +0000

    Flesh out AegirVPS tests, and specify default host and group names.

--- mk/projects/40_aegirvps.mk
+++ mk/projects/40_aegirvps.mk
@@ -2,6 +2,12 @@
 
 init-project-aegirvps-intro:
 	@echo "Initializing Drumkit AegirVPS project."
+# Override default host and group names.
+init-project-aegirvps: group=aegir
+init-project-aegirvps: host=aegir0
+# TODO: override group and host templates
+#init-project-aegirvps: group_template=...
+#init-project-aegirvps: host_template=...
 init-project-aegirvps: init-project-aegirvps-intro init-project-openstack init-project-aegir ## Initialize a project to manage full-stack AegirVPS systems on Openstack. 
 	@echo "Finished initializing Drumkit AegirVPS project."
 
