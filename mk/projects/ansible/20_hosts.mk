920abd83f851e04bcc201c74a5ecf32c90edb951
lamech <danieljasonfriedman@gmail.com>
2019-08-07 17:15:35 +0000

    Allow host vars filename to be overridden.

--- mk/projects/ansible/20_hosts.mk
+++ mk/projects/ansible/20_hosts.mk
@@ -4,12 +4,13 @@ ANSIBLE_HOST_VARS_DIR=$(ANSIBLE_INVENTORY_DIR)/host_vars
 ANSIBLE_HOST_VARS_TEMPLATE=$(FILES_DIR)/ansible/templates/host-vars.yml.j2
 
 host:=example-host
+ANSIBLE_HOST_VARS_FILENAME ?= $(host).yml
 
 .PHONY: ansible-add-host ansible-add-host-vars-file ansible-add-host-playbook ansible-clean-host
 
 ansible-add-host-vars-file: $(ANSIBLE_HOST_VARS_DIR)
 	@echo "Initializing Ansible host variables file."
-	@jinja2 -D host=$(host) -o $(ANSIBLE_HOST_VARS_DIR)/$(host).yml $(ANSIBLE_HOST_VARS_TEMPLATE)
+	@jinja2 -D host=$(host) -D group=$(group) -o $(ANSIBLE_HOST_VARS_DIR)/$(ANSIBLE_HOST_VARS_FILENAME) $(ANSIBLE_HOST_VARS_TEMPLATE)
 
 ansible-add-host-playbook: $(ANSIBLE_HOSTS_PLAYBOOK_DIR)
 	@echo "Generating host playbook."
