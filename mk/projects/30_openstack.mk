4873a6c4d37df56487b3392ecbe95f35843cbcd4
lamech <danieljasonfriedman@gmail.com>
2019-08-08 15:59:47 +0000

    Improve delegation to openstack role.

--- mk/projects/30_openstack.mk
+++ mk/projects/30_openstack.mk
@@ -1,9 +1,10 @@
 # Tasks for initializing infra-openstack projects.
+INIT_PROJECT_OPENSTACK_DEPENDENCIES := roles/consensus.cloud-openstack $(MK_D)/20_project_openstack.mk
 
 init-project-openstack-intro:
 	@echo "Initializing Drumkit OpenStack infrastructure project."
-init-project-openstack: init-project-openstack-intro init-project-ansible roles/consensus.cloud-openstack $(MK_D)/20_project_openstack.mk ## Initialize a project for provisioning cloud infrastructure on an OpenStack provider.
-	@make -s init-role-cloud-openstack
+init-project-openstack: init-project-openstack-intro $(INIT_PROJECT_OPENSTACK_DEPENDENCIES) ## Initialize a project for provisioning cloud infrastructure on an OpenStack provider.
+	@make -s init-role-openstack host=$(host) group=$(group)
 	@echo "Finished initializing Drumkit OpenStack infrastructure project."
 
 $(MK_D)/20_project_openstack.mk: $(MK_D) 
