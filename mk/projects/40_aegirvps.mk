ffc26be027546bffbe74d1d30d534c4d4254b3cf
lamech <danieljasonfriedman@gmail.com>
2019-08-08 16:54:37 +0000

    Clean up and consolidate aegirvps project dependencies and delegation.

--- mk/projects/40_aegirvps.mk
+++ mk/projects/40_aegirvps.mk
@@ -3,11 +3,8 @@
 init-project-aegirvps-intro:
 	@echo "Initializing Drumkit AegirVPS project."
 # Override default host and group names.
-init-project-aegirvps: group=aegir
-init-project-aegirvps: host=aegir0
-# TODO: override group and host templates
-#init-project-aegirvps: group_template=...
-#init-project-aegirvps: host_template=...
-init-project-aegirvps: init-project-aegirvps-intro init-project-openstack init-project-aegir ## Initialize a project to manage full-stack AegirVPS systems on Openstack. 
+#init-project-aegirvps: host=aegir0
+#init-project-aegirvps: group=aegir
+init-project-aegirvps: init-project-aegirvps-intro $(INIT_PROJECT_OPENSTACK_DEPENDENCIES) $(INIT_PROJECT_AEGIR_DEPENDENCIES) ## Initialize a project to manage full-stack AegirVPS systems on Openstack. 
+	@make -s init-role-aegirvps #host=$(host) group=$(group)
 	@echo "Finished initializing Drumkit AegirVPS project."
-
