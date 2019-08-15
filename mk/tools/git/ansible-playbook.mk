3f8bf4a3aad3137900e50c994b7326121140d528
lamech <danieljasonfriedman@gmail.com>
2019-08-15 20:08:37 +0000

    Update ansible-playbook test targets for role development.

--- mk/tools/git/ansible-playbook.mk
+++ mk/tools/git/ansible-playbook.mk
@@ -17,31 +17,23 @@ endif
 
 ANSIBLE_PLAYBOOK_CMD := ansible-playbook $(ANSIBLE_START_AT_TASK) $(ANSIBLE_INVENTORY) $(ANSIBLE_TAGS)
 
-# Below are helpers for development and testing of Ansible roles.
-ifdef ANSIBLE_ROLE_NAME
+# Below are some helpers that are useful for ansible role testing.
+# TODO: migrate to a new ansible role project type.
 
-ANSIBLE_TEST_PLAYBOOK ?= tests/playbook.yml
+ANSIBLE_TEST_INVENTORY ?= tests/inventory
+ANSIBLE_TEST_PLAYBOOK ?= tests/test.yml
+ANSIBLE_TEST_CMD      ?= $(ANSIBLE_PLAYBOOK_CMD) $(ANSIBLE_TEST_PLAYBOOK) --connection=local -i $(ANSIBLE_TEST_INVENTORY)
 
-ansible-playbook-test-idempotence ansible-playbook-test: inventory=tests/inventory
-ansible-playbook-test-idempotence: ansible-playbook ansible-playbook-test
-	@echo "Run the role/playbook again, checking to make sure it's idempotent."
-	$(ANSIBLE_PLAYBOOK_CMD) --connection=local --sudo --extra-vars "$(ANSIBLE_EXTRA_VARS)" $(ANSIBLE_TEST_PLAYBOOK) \
-  | grep -q 'changed=0.*failed=0' \
-  && (echo 'Idempotence test: pass' && exit 0) \
-  || (echo 'Idempotence test: fail' && exit 1)
+ansible-role-test: ansible-playbook
+	@echo "Run the test playbook $(ANSIBLE_TEST_PLAYBOOK)."
+	$(ANSIBLE_TEST_CMD)
 
-ansible-playbook-test: ansible-playbook ansible-roles $(ANSIBLE_ROLES_PATH)/$(ANSIBLE_ROLE_NAME)
-	@echo "Check the role/playbook's syntax."
+ansible-role-check: ansible-playbook
+	@echo "Run test playbook $(ANSIBLE_TEST_PLAYBOOK) with --check to predict changes without actually making any."
+	$(ANSIBLE_TEST_CMD) --check
+
+ansible-playbook-syntax: ansible-playbook
+	@echo "Check the syntax of test playbook $(ANSIBLE_TEST_PLAYBOOK)."
 	$(ANSIBLE_PLAYBOOK_CMD) --syntax-check $(ANSIBLE_TEST_PLAYBOOK)
-	@echo "Run the role/playbook with ansible-playbook."
-	$(ANSIBLE_PLAYBOOK_CMD) --connection=local --sudo --extra-vars "$(ANSIBLE_EXTRA_VARS)" $(ANSIBLE_TEST_PLAYBOOK)
-
-$(ANSIBLE_ROLES_PATH)/$(ANSIBLE_ROLE_NAME): $(ANSIBLE_ROLES_PATH)
-	@echo "Creating symlink to include this role for tests."
-	rm -f $(ANSIBLE_ROLES_PATH)/$(ANSIBLE_ROLE_NAME)
-	cd $(ANSIBLE_ROLES_PATH) && \
-  ln -f -s ../.. $(ANSIBLE_ROLE_NAME)
-
-endif
 
 # vi:syntax=makefile
