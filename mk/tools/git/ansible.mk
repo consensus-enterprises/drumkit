5bd47d245dc5fd7bbf8d72b7f7f86dae00f9e622
lamech <danieljasonfriedman@gmail.com>
2019-08-20 14:51:43 +0000

    Remove Python2 for CI.

--- mk/tools/git/ansible.mk
+++ mk/tools/git/ansible.mk
@@ -1,7 +1,7 @@
 ansible_NAME         = Ansible
 ansible_RELEASE      ?= v2.8.1
 ansible_DOWNLOAD_URL = https://github.com/ansible/ansible.git
-ansible_DEPENDENCIES = python3-paramiko python3-pip python3-yaml python3-jinja2 python3-pycurl
+ansible_DEPENDENCIES = python3-minimal python3-paramiko python3-pip python3-yaml python3-jinja2 python3-pycurl
 ansible_BIN_DIR      = bin
 ansible_PARENT       = ansible
 
