a69ab7e6554d2b31938bacaaef9e12f1b1f5bac9
Christopher Gervais <chris@ergonlogic.com>
2019-10-10 14:03:29 -0400

    Update Composer version.

--- mk/tools/phar/composer.mk
+++ mk/tools/phar/composer.mk
@@ -1,5 +1,5 @@
 composer_NAME         ?= Composer
-composer_RELEASE      ?= 1.4.2
+composer_RELEASE      ?= 1.9.0
 composer_DOWNLOAD_URL ?= https://getcomposer.org/download/$(composer_RELEASE)/composer.phar
 composer_DEPENDENCIES ?= git php5-cli
 
