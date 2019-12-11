fb97625be5a685dcb406ee51b67dac9343918804
Derek Laventure <sensespidey@gmail.com>
2019-12-11 16:49:14 -0500

    Fix typo in tools/docker.mk

--- mk/tools/docker.mk
+++ mk/tools/docker.mk
@@ -1,6 +1,6 @@
 docker:
 	@echo "Installing Docker."
-	@curl -fsSL https://get.docker.co -o get-docker.sh
+	@curl -fsSL https://get.docker.com -o get-docker.sh
 	@sudo sh get-docker.sh
 	@echo "Adding $USER to docker group."
 	@sudo usermod -aG docker $USER
