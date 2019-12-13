28267155a5f7633d7e0b453c5721c36aeb7af5ae
Derek Laventure <sensespidey@gmail.com>
2019-12-12 23:20:02 -0500

    Fix bug in check for jinja2 to ensure it gets installed locally in the Drumkit.

--- mk/tools/jinja2.mk
+++ mk/tools/jinja2.mk
@@ -1,7 +1,12 @@
 jinja2: deps-python
 	@echo "Installing jinja2."
 	@sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq python3-jinja2
-	@pip3 list 2>/dev/null | grep jinja2-cli || pip3 install jinja2-cli --install-option="--install-scripts=$(BIN_DIR)"
+	if test -f "$(BIN_DIR)/jinja2"; then
+	  @echo "Found jinja2 already in your kit."
+	else
+	  @pip3 install jinja2-cli --install-option="--install-scripts=$(BIN_DIR)"
+	  @echo "Installed jinja2 in your kit."
+	fi
 	@which jinja2
 
 clean-jinja2:
