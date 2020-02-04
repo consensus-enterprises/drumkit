ce60bc0129b17ae7abcf87158fb5fba826406020
Christopher Gervais <chris@ergonlogic.com>
2020-02-04 14:54:53 -0500

    Update Packer version.

--- mk/tools/hashicorp/packer.mk
+++ mk/tools/hashicorp/packer.mk
@@ -1,5 +1,5 @@
 packer_NAME         ?= packer
-packer_RELEASE      ?= 1.1.3
+packer_RELEASE      ?= 1.5.1
 packer_DOWNLOAD_URL ?= https://releases.hashicorp.com/$(packer_NAME)/$(packer_RELEASE)/$(packer_NAME)_$(packer_RELEASE)_$(MK_OS)_amd64.zip
 
 # vi:syntax=makefile
