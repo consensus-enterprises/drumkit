916237018199fa11ca1904e9db6c78aacb59b623
lamech <danieljasonfriedman@gmail.com>
2020-02-05 20:39:51 -0500

    Update python dependencies for pip3 ansible install.

--- scripts/packer/scripts/utils.sh
+++ scripts/packer/scripts/utils.sh
@@ -18,6 +18,8 @@ apt-get install -yqq \
   python3-yaml \
   python3-jinja2 \
   python3-apt \
+  python3-setuptools \
+  python3-wheel \
 > /dev/null
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 pip3 install jinja2-cli matrix-client ansible
