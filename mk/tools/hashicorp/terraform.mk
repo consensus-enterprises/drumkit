8dfdeece92f91f86a8d2c8448cdebe44447c188f
Derek Laventure <sensespidey@gmail.com>
2020-02-24 23:42:36 -0500

    Remove extraneous target for terraform tools.

--- mk/tools/hashicorp/terraform.mk
+++ mk/tools/hashicorp/terraform.mk
@@ -3,6 +3,4 @@ terraform_RELEASE				?= 0.12.21
 terraform_FILENAME			?= $(terraform_NAME)_$(terraform_RELEASE)_linux_amd64.zip
 terraform_DOWNLOAD_URL	?= https://releases.hashicorp.com/terraform/$(terraform_RELEASE)/$(terraform_FILENAME)
 
-terraform:
-	@echo "Ensuring Terraform is installed."
-	@which terraform > /dev/null || (wget $(terraform_DOWNLOAD_URL) && unzip $(terraform_FILENAME) && mv terraform .mk/local/bin && rm $(terraform_FILENAME)
+# vi:syntax=makefile
