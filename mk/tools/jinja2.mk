30b5b261537f6c70b812da0b0cbc5e3353b2fbf6
Derek Laventure <sensespidey@gmail.com>
2020-07-08 00:52:33 -0400

    refs #48: auto-activate python environment to get jinja2, in case it was not initialized yet.

--- mk/tools/jinja2.mk
+++ mk/tools/jinja2.mk
@@ -2,8 +2,7 @@ jinja2_installed = $(shell test -f "$(BIN_DIR)/jinja2")
 
 jinja2-real: deps-python
 	@echo "Installing jinja2."
-	@if test -f "$(BIN_DIR)/jinja2"; then (echo "Found jinja2-cli already in your kit."); else (pip install jinja2-cli) && (echo "Installed jinja2-cli in your kit."); fi
-	@which jinja2
+	@if test -f "$(BIN_DIR)/jinja2"; then (echo "Found jinja2-cli already in your kit."); else (source .mk/.local/bin/activate && pip install jinja2-cli) && (echo "Installed jinja2-cli in your kit."); fi
 
 jinja2:
 ifeq ($(jinja2_installed),)
