3a6a517d0c95693d6688d8563258b7a21ef535b2
lamech <danieljasonfriedman@gmail.com>
2020-08-13 18:40:53 +0000

    Fix formatting and typo.

--- files/drupal-project/20_lando.mk
+++ files/drupal-project/20_lando.mk
@@ -3,32 +3,32 @@
 .PHONY: start stop off destroy
 
 start: ## Start Lando containers.
-				@$(MAKE-QUIET) start-real
+	@$(MAKE-QUIET) start-real
 start-real:
-				@$(ECHO) "$(YELLOW)Starting Lando containers. (Be patient. This may take a while.)$(RESET)"
-				lando start $(QUIET)
-				@$(ECHO) "$(YELLOW)Started Lando containers.$(RESET)"
+	@$(ECHO) "$(YELLOW)Starting Lando containers. (Be patient. This may take a while.)$(RESET)"
+	lando start $(QUIET)
+	@$(ECHO) "$(YELLOW)Started Lando containers.$(RESET)"
 
 stop: ## Stop Lando containers.
-				@$(MAKE-QUIET) stop-real
+	@$(MAKE-QUIET) stop-real
 stop-real:
-				@$(ECHO) "$(YELLOW)Stopping Lando containers.$(RESET)"
-				lando stop $(QUIET)
-				@$(ECHO) "$(YELLOW)Stopped Lando containers.$(RESET)"
+	@$(ECHO) "$(YELLOW)Stopping Lando containers.$(RESET)"
+	lando stop $(QUIET)
+	@$(ECHO) "$(YELLOW)Stopped Lando containers.$(RESET)"
 
 off: ## Power off Lando containers (including Traefik proxy)
-				@($MAKE-QUIET) off-real
+	@$(MAKE-QUIET) off-real
 off-real:
-				@$(ECHO) "$(YELLOW)Powering off Lando.$(RESET)"
-				lando poweroff $(QUIET)
-				@$(ECHO) "$(YELLOW)Lando powered off.$(RESET)"
+	@$(ECHO) "$(YELLOW)Powering off Lando.$(RESET)"
+	lando poweroff $(QUIET)
+	@$(ECHO) "$(YELLOW)Lando powered off.$(RESET)"
 
 destroy: ## Destroy Lando containers
-				@$(MAKE-QUIET) destroy-real
+	@$(MAKE-QUIET) destroy-real
 destroy-real:
-				@$(ECHO) "$(YELLOW)Destroying Lando containers.$(RESET)"
-				lando destroy -y $(QUIET)
-				@$(ECHO) "$(YELLOW)Destroyed Lando containers.$(RESET)"
+	@$(ECHO) "$(YELLOW)Destroying Lando containers.$(RESET)"
+	lando destroy -y $(QUIET)
+	@$(ECHO) "$(YELLOW)Destroyed Lando containers.$(RESET)"
 
 rebuild: ## Rebuild Lando containers
 	@$(MAKE-QUIET) rebuild-real
