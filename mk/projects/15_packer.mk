a83f5fa856bb64ec9be003111005e6fb8eb94345
Derek Laventure <sensespidey@gmail.com>
2020-09-10 22:16:49 -0400

    refs #60: convert init-project-packer targets to use Mustache, and rename jinja2 templates.

--- mk/projects/15_packer.mk
+++ mk/projects/15_packer.mk
@@ -5,30 +5,27 @@ PACKER_SH_SCRIPTS = $(PACKER_SH_DIR)/apt.sh $(PACKER_SH_DIR)/cleanup.sh $(PACKER
 init-project-packer-intro:
 	@echo "Initializing Drumkit Packer project."
 
-.drumkit-packer.conf:
+init-project-packer-vars:
 	@echo "Please provide the following information to initialize your Packer scripts:"
-	@read -p "Docker container project name: " project && echo "CONTAINER_PROJECT_NAME=$${project}" >> .drumkit-packer.conf
-	@read -p "Docker container registry URL: " registry_url && echo "CONTAINER_REGISTRY_URL=$${registry_url}" >> .drumkit-packer.conf
+	@read -p "Docker container project name: " project && export CONTAINER_PROJECT_NAME=$${project:-project} && \
+	read -p "Docker container registry URL: " registry_url && export CONTAINER_REGISTRY_URL=$${registry_url:-registry.gitlab.com} && \
+	make -s $(MK_D)/20_ci.mk && \
+	make -s $(PACKER_SH_DIR)/$(CONTAINER_PROJECT_NAME).sh
 
-clean-drumkit-packer.conf:
-	@rm .drumkit-packer.conf
-
-init-project-packer: init-project-packer-intro .drumkit-packer.conf $(MK_D)/20_ci.mk ##@projects Initialize a packer project.
+init-project-packer-static: mustache
 	@mkdir -p $(PACKER_JSON_DIR) $(PACKER_SH_DIR)
 	@echo "Initializing Packer JSON files and scripts."
 	@cp $(FILES_DIR)/packer/json/*.json $(PACKER_JSON_DIR)
-	@export `cat .drumkit-packer.conf | xargs` && make -s \
-		$(PACKER_SH_SCRIPTS) \
-		$(PACKER_SH_DIR)/$$CONTAINER_PROJECT_NAME.sh \
-		$(PACKER_JSON_DIR)/40-$$CONTAINER_PROJECT_NAME.json
-	@make -s clean-drumkit-packer.conf
+	@make -s $(PACKER_SH_SCRIPTS) $(PACKER_JSON_DIR)/40-$$CONTAINER_PROJECT_NAME.json
+
+init-project-packer: init-project-packer-intro init-project-packer-static ##@projects Initialize a packer project.
+	@make -s init-project-packer-vars
 	@echo "Finished initializing Drumkit Packer project."
 
 $(MK_D)/20_ci.mk:
 	@echo "Initializing CI makefile."
-	@echo jinja2 `perl -n < .drumkit-packer.conf -e 'chomp and print " -D " and print "\"$$_\""'` -o $@ $(FILES_DIR)/packer/20_ci.mk.j2 > .drumkit-packer-init-$(notdir $@)-script.cmd
-	@ . .drumkit-packer-init-$(notdir $@)-script.cmd
-	@rm .drumkit-packer-init-$(notdir $@)-script.cmd
+	@mkdir -p $(MK_D)
+	@mustache ENV $(FILES_DIR)/packer/20_ci.mk.tmpl > $@
 
 $(PACKER_JSON_DIR)/40-$(CONTAINER_PROJECT_NAME).json:
 	@echo Initializing project-specific Packer JSON file $@
@@ -39,6 +36,4 @@ $(PACKER_SH_SCRIPTS):
 
 $(PACKER_SH_DIR)/$(CONTAINER_PROJECT_NAME).sh:
 	@echo "Initializing project specific Packer script $@"
-	@echo jinja2 `perl -n < .drumkit-packer.conf -e 'chomp and print " -D " and print "\"$$_\""'` -o $@ $(FILES_DIR)/packer/project.sh.j2 > .drumkit-packer-init-$(notdir $@)-script.cmd
-	@ . .drumkit-packer-init-$(notdir $@)-script.cmd
-	@rm .drumkit-packer-init-$(notdir $@)-script.cmd
+	@mustache ENV $(FILES_DIR)/packer/project.sh.tmpl > $@
