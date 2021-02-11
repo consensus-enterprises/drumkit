ec832dcbaa54f6511e63d39640e26d8d79a22c56
Seonaid <seonaid@consensus.enterprises>
2021-02-11 14:11:36 -0400

    Remove conditions from hugo installation.

--- mk/tools/go/hugo.mk
+++ mk/tools/go/hugo.mk
@@ -5,12 +5,12 @@ local_OS          ?= $(shell uname)
 hugo_NAME         ?= Hugo
 hugo_RELEASE      ?= 0.74.3
 
-ifeq ($(local_OS),Darwin)
-        hugo_OS   ?= macOS
-else
-        hugo_OS   ?= $(local_OS)
-endif
+# ifeq ($(local_OS),Darwin)
+#         hugo_OS   ?= macOS
+# else
+#         hugo_OS   ?= $(local_OS)
+# endif
 
-hugo_DOWNLOAD_URL ?= https://github.com/gohugoio/hugo/releases/download/v$(hugo_RELEASE)/hugo_extended_$(hugo_RELEASE)_$(hugo_OS)-64bit.tar.gz
+hugo_DOWNLOAD_URL ?= https://github.com/gohugoio/hugo/releases/download/v$(hugo_RELEASE)/hugo_extended_$(hugo_RELEASE)_Linux-64bit.tar.gz
 
 # vi:syntax=makefile
