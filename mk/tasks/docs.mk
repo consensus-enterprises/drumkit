a6981f6719d04c0b6340a97b0a483d737a421b15
Seonaid <seonaid@consensus.enterprises>
2021-03-16 12:07:52 -0300

    Add information on running behat tests locally.

--- mk/tasks/docs.mk
+++ mk/tasks/docs.mk
@@ -1,10 +1,10 @@
 # Build and serve our docs site locally.
 
-.PHONY: docs docs-kill
+.PHONY: docs-start docs-kill
 
 HUGO_SERVE_CMD=hugo serve --noHTTPCache --quiet --disableFastRender
 
-docs: ## Run Hugo server locally in the background (http://localhost:1313).
+docs-start: ## Run Hugo server locally in the background (http://localhost:1313).
 	cd docs && $(HUGO_SERVE_CMD) 2>&1 > /dev/null &
 
 #TODO add support to kill local server on a mac
