ae193037211a428fedea379f7ca428708b9cab22
Colan Schwartz <13228-colan@users.noreply.gitlab.com>
2022-09-30 12:21:57 -0400

    Upgraded Terraform.  Assumes no Swift state backend.  See this for details:
    https://gitlab.com/consensus.enterprises/saas/backupscale/infrastructure/-/issues/74

--- mk/tools/hashicorp/terraform.mk
+++ mk/tools/hashicorp/terraform.mk
@@ -1,5 +1,5 @@
 terraform_NAME					?= terraform
-terraform_RELEASE				?= 0.12.21
+terraform_RELEASE				?= 1.3.1
 terraform_FILENAME			?= $(terraform_NAME)_$(terraform_RELEASE)_linux_amd64.zip
 terraform_DOWNLOAD_URL	?= https://releases.hashicorp.com/terraform/$(terraform_RELEASE)/$(terraform_FILENAME)
 
