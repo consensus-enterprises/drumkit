a24dc45c6b7c995d73ecdcd8e5c7ff96a0762ab4
Dan Friedman <dan@consensus.enterprises>
2023-01-11 14:48:12 -0500

    Add Darwin support and upgrade Terraform to 1.3.7. Also undo tab damage.

--- mk/tools/hashicorp/terraform.mk
+++ mk/tools/hashicorp/terraform.mk
@@ -1,6 +1,14 @@
-terraform_NAME					?= terraform
-terraform_RELEASE				?= 1.3.1
-terraform_FILENAME			?= $(terraform_NAME)_$(terraform_RELEASE)_linux_amd64.zip
-terraform_DOWNLOAD_URL	?= https://releases.hashicorp.com/terraform/$(terraform_RELEASE)/$(terraform_FILENAME)
+# We are assuming that all Mac users are on ARM and all Linux users are on AMD64.
+# Override terraform_ARCH if not.
+ifeq ($(local_OS),Darwin)
+    terraform_ARCH ?= arm64
+else
+    terraform_ARCH ?= amd64
+endif
+
+terraform_NAME          ?= terraform
+terraform_RELEASE       ?= 1.3.7
+terraform_FILENAME      ?= $(terraform_NAME)_$(terraform_RELEASE)_$(call lc,$(local_OS))_$(terraform_ARCH).zip
+terraform_DOWNLOAD_URL  ?= https://releases.hashicorp.com/terraform/$(terraform_RELEASE)/$(terraform_FILENAME)
 
 # vi:syntax=makefile
