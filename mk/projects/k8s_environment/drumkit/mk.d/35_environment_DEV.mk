1a7fe65275604b8db4e42d03108aff87293adee7
Christopher Gervais <chris@ergonlogic.com>
2023-01-26 18:53:08 -0500

    Relates to #110: Enable environment initialization.

---
+++ mk/projects/k8s_environment/drumkit/mk.d/35_environment_DEV.mk
@@ -0,0 +1,36 @@
+#########################
+# The '{{ ENVIRONMENT_NAME }}' environment #
+#########################
+
+# This environment will be used to deploy an instance of the app for
+# development purposes. This environment is explicitly volatile, and can be
+# destroyed or rebuilt at any time. Thus, it will run on the '{{ CLUSTER_NAME }}'
+# cluster.
+
+{{ ENVIRONMENT_NAME }}-plan-environment:  ##@envision Print plan for creating '{{ ENVIRONMENT_NAME }}' environment within which to deploy our app.
+	@$(make) .k8s-plan-environment CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
+
+{{ ENVIRONMENT_NAME }}-create-environment: ##@envision Create the '{{ ENVIRONMENT_NAME }}' environment within which to deploy our app.
+	@$(make) .k8s-create-environment CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
+
+{{ ENVIRONMENT_NAME }}-print-resources: ##@envision Print resources from the '{{ ENVIRONMENT_NAME }}' environment environment.
+	@$(make) .k8s-print-resources CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
+
+{{ ENVIRONMENT_NAME }}-use-environment:  ##@envision Use the {{ ENVIRONMENT_NAME }} environment config for Kubernetes.
+	@$(make) .k8s-use-environment ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }} CLUSTER_NAME={{ CLUSTER_NAME }}
+
+{{ ENVIRONMENT_NAME }}-delete-environment: ##@envision Delete the '{{ ENVIRONMENT_NAME }}' environment.
+	@$(make) .k8s-delete-environment CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
+
+#########################
+# '{{ ENVIRONMENT_NAME }}' application     #
+#########################
+
+{{ ENVIRONMENT_NAME }}-plan-app:  ##@envision Print plan for deploying our app to the {{ ENVIRONMENT_NAME }} environment.
+	@$(make) .k8s-plan-app ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }} CLUSTER_NAME={{ CLUSTER_NAME }}
+
+{{ ENVIRONMENT_NAME }}-deploy-app:  ##@envision Deploy our app to the {{ ENVIRONMENT_NAME }} environment.
+	@$(make) .k8s-deploy-app ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }} CLUSTER_NAME={{ CLUSTER_NAME }}
+
+{{ ENVIRONMENT_NAME }}-delete-app:  ##@envision Delete our app from the {{ ENVIRONMENT_NAME }} environment.
+	@$(make) .k8s-delete-app ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }} CLUSTER_NAME={{ CLUSTER_NAME }}
