7aa65c062bb04cf3f6cfda1a6a2e3237964c1955
Christopher Gervais <chris@ergonlogic.com>
2023-01-27 15:40:22 -0500

    Relates to #110: Fix Mustache variables in Drupal app makefile.

--- mk/projects/k8s_drupal_app/drumkit/mk.d/45_drupal_app_DEV.mk
+++ mk/projects/k8s_drupal_app/drumkit/mk.d/45_drupal_app_DEV.mk
@@ -3,10 +3,10 @@
 #############################
 
 {{ DRUPAL_APP_ENVIRONMENT }}-plan-app:  ##@envision Print plan for deploying our app to the {{ DRUPAL_APP_ENVIRONMENT }} environment.
-	@$(make) .k8s-plan-app DRUPAL_APP_ENVIRONMENT={{ DRUPAL_APP_ENVIRONMENT }} CLUSTER_NAME={{ DRUPAL_APP_CLUSTER }}
+	@$(make) .k8s-plan-app ENVIRONMENT_NAME={{ DRUPAL_APP_ENVIRONMENT }} CLUSTER_NAME={{ DRUPAL_APP_CLUSTER }}
 
 {{ DRUPAL_APP_ENVIRONMENT }}-deploy-app:  ##@envision Deploy our app to the {{ DRUPAL_APP_ENVIRONMENT }} environment.
-	@$(make) .k8s-deploy-app DRUPAL_APP_ENVIRONMENT={{ DRUPAL_APP_ENVIRONMENT }} CLUSTER_NAME={{ DRUPAL_APP_CLUSTER }}
+	@$(make) .k8s-deploy-app ENVIRONMENT_NAME={{ DRUPAL_APP_ENVIRONMENT }} CLUSTER_NAME={{ DRUPAL_APP_CLUSTER }}
 
 {{ DRUPAL_APP_ENVIRONMENT }}-delete-app:  ##@envision Delete our app from the {{ DRUPAL_APP_ENVIRONMENT }} environment.
-	@$(make) .k8s-delete-app DRUPAL_APP_ENVIRONMENT={{ DRUPAL_APP_ENVIRONMENT }} CLUSTER_NAME={{ DRUPAL_APP_CLUSTER }}
+	@$(make) .k8s-delete-app ENVIRONMENT_NAME={{ DRUPAL_APP_ENVIRONMENT }} CLUSTER_NAME={{ DRUPAL_APP_CLUSTER }}
