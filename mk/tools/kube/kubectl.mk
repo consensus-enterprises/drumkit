f23fd29598c39b720aa5b3e608ff99a34719c51d
Christopher Gervais <chris@ergonlogic.com>
2023-01-27 15:58:54 -0500

    Update kubectl version to the most recent that we can use at Vexxhost.

--- mk/tools/kube/kubectl.mk
+++ mk/tools/kube/kubectl.mk
@@ -1,5 +1,5 @@
 kubectl_NAME         ?= kubectl
-kubectl_RELEASE      ?= v1.3.0
+kubectl_RELEASE      ?= v1.21.1
 kubectl_DOWNLOAD_URL ?= https://storage.googleapis.com/kubernetes-release/release/$(kubectl_RELEASE)/bin/$(MK_OS)/amd64/$(kubectl_NAME)
 kubectl_DEPENDENCIES ?= 
 
