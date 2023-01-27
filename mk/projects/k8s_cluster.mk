09ac62a8ed7663034fa51e7082d963a0a25ef793
Christopher Gervais <chris@ergonlogic.com>
2023-01-27 15:20:44 -0500

    Relates to #110: Fix output from init-k8s targets.

--- mk/projects/k8s_cluster.mk
+++ mk/projects/k8s_cluster.mk
@@ -23,35 +23,46 @@ K8S_CLUSTER_SYMLINKS = \
     $(K8S_CLUSTER_DIR)/$(K8S_CLUSTER_NAME)/versions.tf
 
 # @TODO: Prompt for the Openstack cloud key during init, and pre-populate these vars.
+init-k8s-cluster: init-k8s-cluster-intro
 init-k8s-cluster: $(K8S_CLUSTER_BASE_FILES)
 init-k8s-cluster: $(K8S_CLUSTER_TEMPLATE_FILES)
 init-k8s-cluster: $(K8S_CLUSTER_SYMLINKS)
 init-k8s-cluster: $(K8S_CLUSTER_DIR)/.gitignore
 init-k8s-cluster: drumkit/mk.d/25_cluster_$(K8S_CLUSTER_NAME).mk
 init-k8s-cluster: ## Initialize configuration and Drumkit targets to create and manage Kubernetes clusters on Openstack.
-	$(ECHO) "To build a cluster, you will need to specify which Openstack cloud"
-	$(ECHO) "you would like to use in 'build/clusters/base/variables.tf'."
-	$(ECHO) "Replace the 'default' for the 'openstack_cloud' variable with the"
-	$(ECHO) "appropriate key from '~/.config/openstack/clouds.yaml'."
-	$(ECHO)
-	$(ECHO) "In addition, you will need to specify this name in 'Makefile' as:"
-	$(ECHO) "    OS_CLOUD = <your-cloud-name>"
-	$(ECHO)
+	@$(ECHO)
+	@$(ECHO) "To build a cluster, you will need to specify which Openstack cloud"
+	@$(ECHO) "you would like to use in 'build/clusters/base/variables.tf'."
+	@$(ECHO) "Replace the 'default' for the 'openstack_cloud' variable with the"
+	@$(ECHO) "appropriate key from '~/.config/openstack/clouds.yaml'."
+	@$(ECHO)
+	@$(ECHO) "In addition, you will need to specify this name in 'Makefile' as:"
+	@$(ECHO) "    OS_CLOUD = <your-cloud-name>"
+	@$(ECHO)
+
+init-k8s-cluster-intro:
+	@$(ECHO) ">>> $(WHITE)Creating '$(K8S_CLUSTER_NAME)' cluster.$(RESET) <<<"
+	@$(ECHO)
 
 $(K8S_CLUSTER_DIR)/.gitignore:
-	mkdir -p $(@D)
-	cp $(K8S_CLUSTER_RESOURCES_DIR)/$(K8S_CLUSTER_DIR)/$(@F) $@
+	@$(ECHO) "$(YELLOW)Creating file: '$(@F)'.$(RESET)"
+	@mkdir -p $(@D)
+	@cp $(K8S_CLUSTER_RESOURCES_DIR)/$(K8S_CLUSTER_DIR)/$(@F) $@
 
 drumkit/mk.d/25_cluster_$(K8S_CLUSTER_NAME).mk:
-	mkdir -p $(@D)
-	CLUSTER_NAME=$(K8S_CLUSTER_NAME) CLUSTER_NAME_LC=$(call lc,$(K8S_CLUSTER_NAME)) mustache ENV $(K8S_CLUSTER_RESOURCES_DIR)/drumkit/mk.d/25_cluster_$(K8S_CLUSTER_DEFAULT_NAME).mk > $@
+	@$(ECHO) "$(YELLOW)Creating makefile: '$(@F)'.$(RESET)"
+	@mkdir -p $(@D)
+	@CLUSTER_NAME=$(K8S_CLUSTER_NAME) CLUSTER_NAME_LC=$(call lc,$(K8S_CLUSTER_NAME)) mustache ENV $(K8S_CLUSTER_RESOURCES_DIR)/drumkit/mk.d/25_cluster_$(K8S_CLUSTER_DEFAULT_NAME).mk > $@
 
 $(K8S_CLUSTER_TEMPLATE_FILES):
-	mkdir -p $(@D)
-	CLUSTER_NAME=$(K8S_CLUSTER_NAME) CLUSTER_NAME_LC=$(call lc,$(K8S_CLUSTER_NAME)) mustache ENV $(K8S_CLUSTER_TEMPLATE_DIR)/$(@F) > $@
+	@$(ECHO) "$(YELLOW)Creating file: '$(@F)'.$(RESET)"
+	@mkdir -p $(@D)
+	@CLUSTER_NAME=$(K8S_CLUSTER_NAME) CLUSTER_NAME_LC=$(call lc,$(K8S_CLUSTER_NAME)) mustache ENV $(K8S_CLUSTER_TEMPLATE_DIR)/$(@F) > $@
 $(K8S_CLUSTER_BASE_FILES):
-	mkdir -p $(@D)
-	CLUSTER_NAME=$(K8S_CLUSTER_NAME) CLUSTER_NAME_LC=$(call lc,$(K8S_CLUSTER_NAME)) mustache ENV $(K8S_CLUSTER_RESOURCES_DIR)/$@ > $@
+	@$(ECHO) "$(YELLOW)Creating file: '$(@F)'.$(RESET)"
+	@mkdir -p $(@D)
+	@CLUSTER_NAME=$(K8S_CLUSTER_NAME) CLUSTER_NAME_LC=$(call lc,$(K8S_CLUSTER_NAME)) mustache ENV $(K8S_CLUSTER_RESOURCES_DIR)/$@ > $@
 $(K8S_CLUSTER_SYMLINKS):
-	mkdir -p $(@D)
-	ln -s ../base/$(@F) $@
+	@$(ECHO) "$(YELLOW)Creating symlink: '$(@F)'.$(RESET)"
+	@mkdir -p $(@D)
+	@ln -s ../base/$(@F) $@
