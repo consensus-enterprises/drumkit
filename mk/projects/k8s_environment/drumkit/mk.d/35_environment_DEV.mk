4edac51f1c306aa5e21c2fb923bdb45a3524f488
Dan Friedman <dan@consensus.enterprises>
2023-02-02 15:54:56 -0500

    Relates to #110: Clean up @envision tagging in Kubernetes related targets.

--- mk/projects/k8s_environment/drumkit/mk.d/35_environment_DEV.mk
+++ mk/projects/k8s_environment/drumkit/mk.d/35_environment_DEV.mk
@@ -7,18 +7,18 @@
 # destroyed or rebuilt at any time. Thus, it will run on the '{{ CLUSTER_NAME }}'
 # cluster.
 
-{{ ENVIRONMENT_NAME }}-plan-environment:  ##@envision Print plan for creating '{{ ENVIRONMENT_NAME }}' environment within which to deploy our app.
+{{ ENVIRONMENT_NAME }}-plan-environment:  ##@{{ PROJECT_NAME }} Print plan for creating '{{ ENVIRONMENT_NAME }}' environment within which to deploy our app.
 	@$(make) .k8s-plan-environment CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
 
-{{ ENVIRONMENT_NAME }}-create-environment: ##@envision Create the '{{ ENVIRONMENT_NAME }}' environment within which to deploy our app.
+{{ ENVIRONMENT_NAME }}-create-environment: ##@{{ PROJECT_NAME }} Create the '{{ ENVIRONMENT_NAME }}' environment within which to deploy our app.
 	@$(make) .k8s-create-environment CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
 
-{{ ENVIRONMENT_NAME }}-print-resources: ##@envision Print resources from the '{{ ENVIRONMENT_NAME }}' environment environment.
+{{ ENVIRONMENT_NAME }}-print-resources: ##@{{ PROJECT_NAME }} Print resources from the '{{ ENVIRONMENT_NAME }}' environment environment.
 	@$(make) .k8s-print-resources CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
 
-{{ ENVIRONMENT_NAME }}-use-environment:  ##@envision Use the {{ ENVIRONMENT_NAME }} environment config for Kubernetes.
+{{ ENVIRONMENT_NAME }}-use-environment:  ##@{{ PROJECT_NAME }} Use the {{ ENVIRONMENT_NAME }} environment config for Kubernetes.
 	@$(make) .k8s-use-environment ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }} CLUSTER_NAME={{ CLUSTER_NAME }}
 
-{{ ENVIRONMENT_NAME }}-delete-environment: ##@envision Delete the '{{ ENVIRONMENT_NAME }}' environment.
+{{ ENVIRONMENT_NAME }}-delete-environment: ##@{{ PROJECT_NAME }} Delete the '{{ ENVIRONMENT_NAME }}' environment.
 	@$(make) .k8s-delete-environment CLUSTER_NAME={{ CLUSTER_NAME }} ENVIRONMENT_NAME={{ ENVIRONMENT_NAME }}
 
