ed96ad2fd6aca7b8cb53ada74d113c2bd398a4f9
Dan Friedman <dan@consensus.enterprises>
2023-02-07 17:02:05 -0500

    Relates to #110: enable certificate generation and domain resolution in k8s Drupal apps.

--- mk/projects/k8s_drupal_app.mk
+++ mk/projects/k8s_drupal_app.mk
@@ -4,12 +4,19 @@ include $(SELF_DIR)k8s_drupal_app/drupal-apps.mk
 K8S_DRUPAL_APP_RESOURCES_DIR =.mk/mk/projects/k8s_drupal_app
 K8S_DRUPAL_APP_TEMPLATE_DIR  = $(K8S_DRUPAL_APP_RESOURCES_DIR)/$(K8S_DRUPAL_APP_DIR)/$(K8S_ENVIRONMENT_DEFAULT_NAME)
 K8S_ENVIRONMENT_NAME := DEV
+K8S_HOSTING_DEFAULT_DOMAIN = aegir.dev
+K8S_HOSTING_DOMAIN := $(K8S_HOSTING_DEFAULT_DOMAIN)
+K8S_HOSTING_DEFAULT_EMAIL_DOMAIN ?= consensus.enterprises
+K8S_HOSTING_EMAIL_DOMAIN := $(K8S_HOSTING_DEFAULT_EMAIL_DOMAIN)
 K8S_DRUPAL_APP_DIR = build/app
 K8S_DRUPAL_APP_DRUMKIT_PREFIX= 45_drupal_app
 
 # This list of vars will be passed to all templating operations below:
 K8S_DRUPAL_APP_TEMPLATE_VARS = \
     PROJECT_NAME=$(PROJECT_NAME) \
+    CLIENT_NAME=$(CLIENT_NAME) \
+    HOSTING_DOMAIN=$(K8S_HOSTING_DOMAIN) \
+    HOSTING_EMAIL_DOMAIN=$(K8S_HOSTING_EMAIL_DOMAIN) \
     DRUPAL_APP_ENVIRONMENT=$(K8S_ENVIRONMENT_NAME) \
     DRUPAL_APP_ENVIRONMENT_LC=$(call lc,$(K8S_ENVIRONMENT_NAME)) \
     DRUPAL_APP_CLUSTER=$(K8S_CLUSTER_NAME) \
@@ -17,6 +24,7 @@ K8S_DRUPAL_APP_TEMPLATE_VARS = \
 
 K8S_DRUPAL_APP_FILES = \
     $(K8S_DRUPAL_APP_DIR)/base/app-variables.yaml \
+    $(K8S_DRUPAL_APP_DIR)/base/cert-manager.yaml \
     $(K8S_DRUPAL_APP_DIR)/base/component-drupal.yaml \
     $(K8S_DRUPAL_APP_DIR)/base/component-mariadb.yaml \
     $(K8S_DRUPAL_APP_DIR)/base/ingress-service.yaml \
@@ -25,9 +33,10 @@ K8S_DRUPAL_APP_FILES = \
     $(K8S_DRUPAL_APP_DIR)/base/registry-credentials.yaml
 
 K8S_DRUPAL_APP_TEMPLATE_FILES = \
-    $(K8S_DRUPAL_APP_DIR)/$(K8S_ENVIRONMENT_NAME)/kustomization.yaml \
     $(K8S_DRUPAL_APP_DIR)/$(K8S_ENVIRONMENT_NAME)/app-secrets.yaml \
-    $(K8S_DRUPAL_APP_DIR)/$(K8S_ENVIRONMENT_NAME)/app-variables.patch.yaml
+    $(K8S_DRUPAL_APP_DIR)/$(K8S_ENVIRONMENT_NAME)/app-variables.patch.yaml \
+    $(K8S_DRUPAL_APP_DIR)/$(K8S_ENVIRONMENT_NAME)/ingress-service.patch.yaml \
+    $(K8S_DRUPAL_APP_DIR)/$(K8S_ENVIRONMENT_NAME)/kustomization.yaml
 
 K8S_DRUPAL_APP_DRUMKIT_FILES= \
     drumkit/mk.d/$(K8S_DRUPAL_APP_DRUMKIT_PREFIX)_$(K8S_ENVIRONMENT_NAME).mk
@@ -47,8 +56,8 @@ init-k8s-drupal-app: ## Initialize configuration and Drumkit targets to create a
 	$(ECHO) "You should customize the site name and install profile in"
 	$(ECHO) "'build/app/base/app-variables.yaml'"
 	$(ECHO)
-	$(ECHO) "You should enable automatic HTTPS certificate generation (via Let's Encrypt)"
-	$(ECHO) "by un-commenting the appropriate line in"
+	$(ECHO) "Automatic HTTPS certificate generation is now enabled using"
+	$(ECHO) "the Let's Encrypt Staging server. You can switch this to production in 
 	$(ECHO) "'build/app/base/ingress-service.yaml'"
 	$(ECHO)
 	$(ECHO) "You should update the documentation in the following files to reflect"
