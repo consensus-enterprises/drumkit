4fec0b74cc9bfe0fedcf06b67d9367049da1031d
Dan Friedman <dan@consensus.enterprises>
2023-02-24 15:37:01 -0500

    Relates to #121: Ensure we identify and remove symlinks as well as files in .remove.

--- mk/tasks/remove.mk
+++ mk/tasks/remove.mk
@@ -2,9 +2,9 @@
 
 .remove: ## [FILES_TO_REMOVE] Delete a list of files.
 	@for FILE_TO_REMOVE in $(FILES_TO_REMOVE); do \
-            if [ -e "$$FILE_TO_REMOVE" ]; then \
+            if [ -e "$$FILE_TO_REMOVE" ] || [ -L "$$FILE_TO_REMOVE" ]; then \
                 echo -e "$(YELLOW)Removing file: '$$FILE_TO_REMOVE'.$(RESET)"; \
-                rm -f "$$FILE_TO_REMOVE"; \
+                rm "$$FILE_TO_REMOVE"; \
             else \
                 echo -e "$(ORANGE)Not removing non-existent file: '$$FILE_TO_REMOVE'.$(RESET)"; \
             fi \
