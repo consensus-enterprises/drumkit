ee44233f75e8fcd2a05fac5c89fe4abfa591629d
Dan Friedman <dan@consensus.enterprises>
2023-03-08 14:56:55 -0500

    Relates to #123: Add drupal app redeployment target.

--- mk/projects/k8s_project/drupal_projects.mk
+++ mk/projects/k8s_project/drupal_projects.mk
@@ -6,18 +6,23 @@
 # connect to our PVCs. So, during a rollout, a new pod will fail, because the
 # existing pod is attached to the PVC. Once we have `csi-manila-cephfs`-backed
 # PVCs, we should be able to do a rollout for (multiple) application pods.
-.redeploy-drupal-app: ##@{{ PROJECT_NAME }} Deploy the latest release to the {{ DRUPAL_APP_ENVIRONMENT }} environment
-	@$(ECHO) "$(YELLOW)==> You are about to redeploy the {{ DRUPAL_APP_ENVIRONMENT }} environment.$(RESET)"
+.redeploy-drupal-app: .redeploy-drupal-app-intro
+.redeploy-drupal-app: .confirm-proceed
+.redeploy-drupal-app-intro: 
+	@$(ECHO) "$(YELLOW)==> You are about to redeploy the $(DRUPAL_APP_ENVIRONMENT) environment.$(RESET)"
 	@$(ECHO) "$(ORANGE)==> Did you remember to take a backup?$(RESET)"
-	@echo -n "Are you sure? [y/N] " && read ans && [ $${ans:-N} = y ]
-	@$(ECHO) "$(YELLOW)==> Switching to {{ DRUPAL_APP_ENVIRONMENT }} environment.$(RESET)"
-	make {{ DRUPAL_APP_ENVIRONMENT }}-use-environment
+
+.redeploy-drupal-app:
+	@$(ECHO) "$(YELLOW)==> Switching to $(DRUPAL_APP_ENVIRONMENT) environment.$(RESET)"
+	$(make) $(DRUPAL_APP_ENVIRONMENT)-use-environment
 	@$(ECHO) "$(YELLOW)==> Deleting Drupal deployment.$(RESET)"
-	kubectl delete deployment/drupal-deployment
+	$(kubectl) delete deployment/drupal-deployment
 	@$(ECHO) "$(YELLOW)==> Re-deploying Drupal component.$(RESET)"
-	make {{ DRUPAL_APP_ENVIRONMENT }}-deploy-app
+	$(make) $(DRUPAL_APP_ENVIRONMENT)-deploy-app
 	@$(ECHO) "$(YELLOW)==> Running Drupal database updates.$(RESET)"
 	@$(make) run-command-on-drupal-pod REMOTE_COMMAND="bin/drush updatedb:status"
 	@echo -n "Proceed with database updates? [y/N] " && read ans && [ $${ans:-N} = y ]
 	@$(make) run-command-on-drupal-pod REMOTE_COMMAND="bin/drush updatedb"
 	@$(make) run-command-on-drupal-pod REMOTE_COMMAND="bin/drush status"
+
+
