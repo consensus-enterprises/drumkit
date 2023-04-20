6924dd1fe9e5e5a52ead8757d2e8926eb491f1b4
Dan Friedman <dan@consensus.enterprises>
2023-04-20 19:19:21 +0000

    Relates to #126: Add Docker and DDEV layers for CI.

--- drumkit/mk.d/20_ci.mk
+++ drumkit/mk.d/20_ci.mk
@@ -1,5 +1,6 @@
-CONTAINER_REGISTRY_URL ?= registry.gitlab.com/consensus.enterprises/drumkit
-CONTAINER_PROJECT_NAME = drumkit
+DRUMKIT_CONTAINER_REGISTRY_URL ?= registry.gitlab.com/consensus.enterprises/drumkit
+CONTAINER_PROJECT_NAME          = drumkit
+DRUMKIT_DOCKER_IMAGES           = docker/focal docker/base docker/docker docker/ddev
 
 local_ref = $(shell git rev-parse HEAD)
 clone_ref = $(shell [ -d .clone ] && (cd .clone && git rev-parse HEAD))
@@ -14,18 +15,22 @@ else
 	@echo ".clone directory is up to date, skipping reclone."
 endif
 
+docker-login:
+	docker login registry.gitlab.com
+
 ci-image: packer
 	@if [ -z ${CONTAINER_SCRIPT} ]; then echo -e "$(YELLOW)Missing required variable $(GREY)CONTAINER_SCRIPT$(YELLOW).$(RESET)"; exit 1; fi
 	@if [ -z ${CONTAINER_PROJECT_NAME} ]; then echo -e "$(YELLOW)Missing required variable $(GREY)CONTAINER_PROJECT_NAME$(YELLOW).$(RESET)"; exit 1; fi
 	@if [ -z ${CONTAINER_REGISTRY_URL} ]; then echo -e "$(YELLOW)Missing required variable $(GREY)CONTAINER_REGISTRY_URL$(YELLOW).$(RESET)"; exit 1; fi
 	@echo "Building packer image for CI: $(CONTAINER_SCRIPT)"
+	@echo "Using project name: $(CONTAINER_PROJECT_NAME)"
+	@echo "Using container registry: $(DRUMKIT_CONTAINER_REGISTRY_URL)"
 	@packer build $(CONTAINER_SCRIPT)
 
-ci-images: clone
-	@echo "Building packer images for CI."
-	@echo "Using project name: $(CONTAINER_PROJECT_NAME)"
-	@echo "Using container registry: $(CONTAINER_REGISTRY_URL)"
-	@for script in `ls scripts/packer/json/*.json`; do make -s ci-image CONTAINER_SCRIPT=$$script CONTAINER_PROJECT_NAME=$(CONTAINER_PROJECT_NAME) CONTAINER_REGISTRY_URL=$(CONTAINER_REGISTRY_URL); done
+ci-images: $(DRUMKIT_DOCKER_IMAGES)
+
+$(DRUMKIT_DOCKER_IMAGES): packer clone
+	@make -s ci-image CONTAINER_SCRIPT=scripts/packer/$@.json CONTAINER_PROJECT_NAME=$(CONTAINER_PROJECT_NAME) CONTAINER_REGISTRY_URL=$(DRUMKIT_CONTAINER_REGISTRY_URL)
 
 ci-local: gitlab-runner
-	gitlab-runner exec docker tests
+	gitlab-runner exec docker --docker-privileged tests
