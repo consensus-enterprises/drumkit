1b4e691fc54eb009f50473dc8ceec54c0bdf1522
Dan Friedman <dan@consensus.enterprises>
2025-01-28 17:27:27 +0000

    Closes #141: upgrade hugo version, adjust to updated hugo distribution URL scheme.

--- mk/tools/go/hugo.mk
+++ mk/tools/go/hugo.mk
@@ -1,16 +1,15 @@
-# We can't use MK_OS here because we need specific capitalization in the URLs we craft below,
+# We can't use MK_OS here because hugo uses inconsistent capitalization in their distribution URLs,
 # and we can't use OS because it is not set yet when this file loads:
 local_OS          ?= $(shell uname)
 
 hugo_NAME         ?= Hugo
-hugo_RELEASE      ?= 0.94.1
+hugo_RELEASE      ?= 0.142.0
 
 ifeq ($(local_OS),Darwin)
-        hugo_OS   ?= macOS
+        hugo_OS   = darwin-universal
 else
-        hugo_OS   ?= $(local_OS)
+# We assume Linux if not Mac.
+        hugo_OS   = Linux-64bit
 endif
 
-hugo_DOWNLOAD_URL ?= https://github.com/gohugoio/hugo/releases/download/v$(hugo_RELEASE)/hugo_extended_$(hugo_RELEASE)_$(hugo_OS)-64bit.tar.gz
-
-# vi:syntax=makefile
+hugo_DOWNLOAD_URL ?= https://github.com/gohugoio/hugo/releases/download/v$(hugo_RELEASE)/hugo_$(hugo_RELEASE)_$(hugo_OS).tar.gz
