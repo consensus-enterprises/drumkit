b5ec1f8b7d44881efb877c2a5a91ac530ce6d581
Christopher Gervais <chris@ergonlogic.com>
2025-06-09 18:25:56 -0400

    Relates to #143: Upgrade Packer to latest version and install Docker plugin.

--- mk/tools/hashicorp/packer.mk
+++ mk/tools/hashicorp/packer.mk
@@ -1,5 +1,9 @@
 packer_NAME         ?= packer
-packer_RELEASE      ?= 1.6.0
+packer_RELEASE      ?= 1.13.1
 packer_DOWNLOAD_URL ?= https://releases.hashicorp.com/$(packer_NAME)/$(packer_RELEASE)/$(packer_NAME)_$(packer_RELEASE)_$(MK_OS)_amd64.zip
+
+packer: install-docker-plugin
+install-docker-plugin:
+	packer plugins install github.com/hashicorp/docker
 
 # vi:syntax=makefile
