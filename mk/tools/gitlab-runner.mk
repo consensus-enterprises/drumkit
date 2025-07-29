08f2f118e3bf03bd15f1799f99b72a83930034d8
Christopher Gervais <chris@ergonlogic.com>
2025-07-29 11:52:14 -0400

    Relates to #138: Replace gitlab-runner with gitlab-ci-local.

--- mk/tools/gitlab-runner.mk
+++ mk/tools/gitlab-runner.mk
@@ -1,28 +1,3 @@
-local_OS          ?= $(shell uname)
-ifeq ($(local_OS),Darwin)
-        URL_OS   ?= darwin
-else
-        URL_OS   ?= linux
-endif
-
-GITLAB_RUNNER_URL=https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-$(URL_OS)-amd64
-GITLAB_RUNNER=$(BIN_DIR)/gitlab-runner
-
-gitlab-runner: ## Install and configure local gitlab-runner.
-	@which gitlab-runner || make -s gitlab-runner-real gitlab-runner-config
-
-gitlab-runner-real: docker
-	@echo "Downloading gitlab-runner from $(GITLAB_RUNNER_URL)"
-	@mkdir -p $(BIN_DIR)
-	@curl -L --output $(GITLAB_RUNNER) $(GITLAB_RUNNER_URL) 
-	@chmod 755 $(GITLAB_RUNNER)
-	@which gitlab-runner
-	@$(GITLAB_RUNNER) --version
-
-gitlab-runner-config:
-	@echo "Configuring gitlab-runner."
-	@printf "[[runners]]\n\
-		clone_url = $(PROJECT_ROOT)\n\
-[[runners.docker]]\n\
-		pull_policy = if-not-present\n" > .config.toml
-
+gitlab-runner: ## DEPRECATED. Use gitlab-ci-local instead.
+	$(ECHO) "'gitlab-runner' has been deprecated."
+	$(ECHO) "Use 'gitlab-ci-local' instead ('make gitlab-ci-local')."
