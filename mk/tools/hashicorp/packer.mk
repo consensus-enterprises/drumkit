a88ac2673652dc316d6b5320c5b7b8e1224074dc
Christopher Gervais <chris@ergonlogic.com>
2025-08-27 19:51:56 -0400

    Fix 'packer' target.

--- mk/tools/hashicorp/packer.mk
+++ mk/tools/hashicorp/packer.mk
@@ -2,8 +2,7 @@ packer_NAME         ?= packer
 packer_RELEASE      ?= 1.13.1
 packer_DOWNLOAD_URL ?= https://releases.hashicorp.com/$(packer_NAME)/$(packer_RELEASE)/$(packer_NAME)_$(packer_RELEASE)_$(MK_OS)_amd64.zip
 
-packer: install-docker-plugin
-install-docker-plugin:
+packer-docker-plugin:
 	packer plugins install github.com/hashicorp/docker
 
 # vi:syntax=makefile
