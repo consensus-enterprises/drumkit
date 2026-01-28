838b3227d510336b57e3f1972189a39bd42c95d3
Christopher Gervais <chris@ergonlogic.com>
2026-01-27 22:33:57 -0500

    Revert "feat: Update to new Mustache repo with support for multiple architectures"
    This reverts commit 5d9a47593c68c3c2bc9f63c8362f806c6b66bccf.

--- mk/tools/mustache.mk
+++ mk/tools/mustache.mk
@@ -1,27 +1,25 @@
 mustache_NAME         ?= mustache
-mustache_RELEASE      ?= 1.4.0
+mustache_RELEASE      ?= v1.0.0
 mustache_OS           ?= $(shell echo "$(OS)" | tr '[:upper:]' '[:lower:]')
-mustache_ARCH         ?= $(shell uname -m)
-mustache_DOWNLOAD_URL ?= https://github.com/cbroglie/mustache/releases/download/v$(mustache_RELEASE)/mustache_$(mustache_RELEASE)_$(mustache_OS)_$(mustache_ARCH).tar.gz
-mustache_SRC_DIR      ?= $(SRC_DIR)/$(mustache_NAME)/$(mustache_RELEASE)
-mustache_TAR          ?= $(mustache_NAME)-$(mustache_RELEASE)-$(mustache_OS).tar.gz
+mustache_DOWNLOAD_URL ?= https://github.com/quantumew/mustache-cli/releases/download/$(mustache_RELEASE)/mustache-cli-$(mustache_OS)-amd64.zip
+mustache_ZIP          ?= $(mustache_NAME)-$(mustache_RELEASE)-$(mustache_OS).zip
 
 .PHONY: mustache install-mustache clean-mustache
 
 install-mustache: init-mk $(BIN_DIR)/mustache
 mustache: install-mustache
 
-$(BIN_DIR)/mustache: $(mustache_SRC_DIR)/$(mustache_NAME)
+$(BIN_DIR)/mustache: $(SRC_DIR)/$(mustache_NAME)/$(mustache_RELEASE)/$(mustache_NAME)
 	@echo "Installing mustache $(mustache_RELEASE)."
-	@ln -sf $(mustache_SRC_DIR)/$(mustache_NAME) $@
+	@ln -sf $(SRC_DIR)/$(mustache_NAME)/$(mustache_RELEASE)/$(mustache_NAME) $@
 	@cd $(PROJECT_ROOT) && unset DRUMKIT && source d && \
 	  mustache --version
 
-$(mustache_SRC_DIR)/$(mustache_NAME):
+$(SRC_DIR)/$(mustache_NAME)/$(mustache_RELEASE)/$(mustache_NAME):
 	@echo "Downloading mustache release $(mustache_RELEASE)."
-	@mkdir -p $(mustache_SRC_DIR)
-	@cd $(mustache_SRC_DIR) && curl -o $(mustache_TAR) -sSL $(mustache_DOWNLOAD_URL) && tar -xzf $(mustache_TAR) > /dev/null
+	@mkdir -p $(SRC_DIR)/$(mustache_NAME)/$(mustache_RELEASE)
+	@cd $(SRC_DIR)/$(mustache_NAME)/$(mustache_RELEASE) && curl -o $(mustache_ZIP) -sSL $(mustache_DOWNLOAD_URL) && unzip $(mustache_ZIP) > /dev/null
 
 clean-mustache:
 	@echo "Cleaning mustache."
-	@rm -rf $(mustache_SRC_DIR) $(BIN_DIR)/mustache
+	@rm -rf $(SRC_DIR)/$(mustache_NAME)/$(mustache_RELEASE) $(BIN_DIR)/mustache
