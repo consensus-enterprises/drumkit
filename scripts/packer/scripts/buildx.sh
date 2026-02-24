94ee9b6ab4708cd7ac04166cd6637823d0c13877
Scott Zhu Reeves <8812772-star-szr@users.noreply.gitlab.com>
2026-02-24 17:01:17 -0500

    refactor: Install buildx plugin directly in docker.sh

--- scripts/packer/scripts/buildx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -eux
-
-# Install docker buildx plugin.
-
-export DEBIAN_FRONTEND=noninteractive
-apt-get update -yqq
-apt-get install -yqq \
-  docker-buildx-plugin \
-> /dev/null
--- scripts/packer/scripts/docker.sh
+++ scripts/packer/scripts/docker.sh
@@ -14,5 +14,7 @@ echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docke
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update -yqq > /dev/null
-apt-get install -yqq docker-ce-cli > /dev/null
-
+apt-get install -yqq \
+  docker-ce-cli \
+  docker-buildx-plugin \
+> /dev/null
