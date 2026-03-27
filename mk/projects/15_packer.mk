39e06eb8317ccb1c807c90d1742e0e6189786ddd
Derek Laventure <sensespidey@gmail.com>
2026-03-26 22:34:26 -0400

    refs #154: Reorder init-project-packer sub-targets to prepare for refactoring.

--- mk/projects/15_packer.mk
+++ mk/projects/15_packer.mk
@@ -2,15 +2,12 @@ PACKER_JSON_DIR = scripts/packer/json
 PACKER_SH_DIR = scripts/packer/scripts
 PACKER_SH_SCRIPTS = $(PACKER_SH_DIR)/apt.sh $(PACKER_SH_DIR)/cleanup.sh $(PACKER_SH_DIR)/php.sh $(PACKER_SH_DIR)/purge-extra-packages.sh  $(PACKER_SH_DIR)/python.sh $(PACKER_SH_DIR)/utils.sh
 
+init-project-packer: init-project-packer-intro init-project-packer-static ##@projects Initialize a packer project.
+	@make -s init-project-packer-vars 
+	@echo "Finished initializing Drumkit Packer project."
+
 init-project-packer-intro:
 	@echo "Initializing Drumkit Packer project."
-
-init-project-packer-vars:
-	@echo "Please provide the following information to initialize your Packer scripts:"
-	@read -p "Docker container project name: " project && export CONTAINER_PROJECT_NAME=$${project:-project} && \
-	read -p "Docker container registry URL: " registry_url && export CONTAINER_REGISTRY_URL=$${registry_url:-registry.gitlab.com} && \
-	make -s $(MK_D)/20_ci.mk && \
-	make -s $(PACKER_SH_DIR)/$(CONTAINER_PROJECT_NAME).sh
 
 init-project-packer-static:
 	@mkdir -p $(PACKER_JSON_DIR) $(PACKER_SH_DIR)
@@ -18,9 +15,18 @@ init-project-packer-static:
 	@cp $(FILES_DIR)/packer/json/*.json $(PACKER_JSON_DIR)
 	@make -s $(PACKER_SH_SCRIPTS) $(PACKER_JSON_DIR)/40-$$CONTAINER_PROJECT_NAME.json
 
-init-project-packer: init-project-packer-intro init-project-packer-static ##@projects Initialize a packer project.
-	@make -s init-project-packer-vars 
-	@echo "Finished initializing Drumkit Packer project."
+init-project-packer-vars-new:
+	@echo "Please provide the following information to initialize your Packer scripts:"
+	CONTAINER_PROJECT_NAME ?= $(call prompt,$(CONTAINER_PROJECT_NAME_PROMPT), "project-name")
+	CONTAINER_REGISTRY_URL ?= $(call prompt,$(CONTAINER_REGISTRY_URL_PROMPT), "registry.gitlab.com")
+	make -s $(PACKER_SH_DIR)/$(CONTAINER_PROJECT_NAME).sh
+
+init-project-packer-vars:
+	@echo "Please provide the following information to initialize your Packer scripts:"
+	@read -p "Docker container project name: " project && export CONTAINER_PROJECT_NAME=$${project:-project} && \
+	read -p "Docker container registry URL: " registry_url && export CONTAINER_REGISTRY_URL=$${registry_url:-registry.gitlab.com} && \
+	make -s $(MK_D)/20_ci.mk && \
+	make -s $(PACKER_SH_DIR)/$(CONTAINER_PROJECT_NAME).sh
 
 $(MK_D)/20_ci.mk: ##@testing Create .gitlab-ci.yml file for new 
 	@echo "Initializing CI makefile."
